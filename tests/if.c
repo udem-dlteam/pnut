f2b8b0e55cbe569d0b494304fac4e95a4ab2bf2f
Laurent Huberdeau <laurenthuberd@gmail.com>
2023-12-07 10:53:44 -0500

    Add if/else test

---
+++ tests/if.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+void main() {
+
+  /* Tests basic boolean equality */
+
+  if (TRUE = TRUE) {
+    putchar('t');
+  } else {
+    putchar('f');
+  }
+}
