46060aba5ec5c95daa00ae1875a990311f5f10bc
Marc Feeley <feeley@iro.umontreal.ca>
2023-12-12 12:05:07 -0500

    Add simple compiler based on SIX

--- six-cc.scm
+++ six-cc.scm
@@ -1,83 +1,172 @@
 #!/usr/bin/env gsi
 
+(define support-addr-of? #t)
+
+(define-type ctx
+  glo-decls)
+
+(define (ctx-add-glo-decl! ctx decl)
+  (ctx-glo-decls-set! ctx (cons decl (ctx-glo-decls ctx))))
+
+(define (global-var ident)
+  (string-append "_" (symbol->string (cadr ident))))
+
+(define (global-ref ident)
+  (if support-addr-of?
+      (string-append "_$" (global-var ident))
+      (global-var ident)))
+
+(define (comp-glo-decl ctx ast)
+  (case (car ast)
+
+    ((six.define-variable)
+     (comp-glo-define-variable ctx ast))
+
+    ((six.define-procedure)
+     (comp-glo-define-procedure ctx ast))
+
+    (else
+     (error "unknown global declaration" ast))))
+
+(define (comp-constant ctx ast)
+  (case (car ast)
+    ((six.literal)
+     (let ((val (cadr ast)))
+       (cond ((exact-integer? val)
+              (number->string val))
+             (else
+              "unknown literal" ast))))
+    (else
+     (error "unknown constant" ast))))
+
+(define (comp-glo-define-variable ctx ast)
+  (let* ((name (cadr ast))
+         (type (caddr ast))
+         (dims (cadddr ast))
+         (init (car (cddddr ast))))
+    (if (pair? dims)
+        (error "arrays not yet supported")
+        (let ((val (if init (comp-constant ctx init) "0")))
+          (ctx-add-glo-decl!
+           ctx
+           (list "defglo " (global-var name) " " val))))))
+
+(define (comp-glo-define-procedure ctx ast)
+  (let* ((name (cadr ast))
+         (proc (caddr ast))
+         (result-type (cadr proc))
+         (parameters (caddr proc)))
+    (print "#") (write ast) (newline)))
+
+(define (comp-glo-decl-list ctx ast)
+  (if (pair? ast)
+      (let ((decl (car ast)))
+        (if (eq? (car decl) 'six.x=y) ;; allow assignments at top level
+            (comp-assignment ctx decl)
+            (comp-glo-decl ctx decl))
+        (comp-glo-decl-list ctx (cdr ast)))))
+
+(define (comp-assignment ctx ast)
+  (let ((lhs (cadr ast))
+        (rhs (caddr ast)))
+    (case (car lhs)
+      ((six.identifier)
+       (ctx-add-glo-decl!
+        ctx
+        (list ": $(( " (global-ref lhs) " = " (comp-rvalue ctx rhs) " ))")))
+      (else
+       (error "unknown lhs" lhs)))))
+
+(define (comp-lvalue ctx ast)
+  (case (car ast)
+    ((six.identifier)
+     (global-ref ast))
+    (else
+     (error "unknown lvalue" ast))))
+
+(define (comp-rvalue ctx ast)
+  (case (car ast)
+    ((six.literal)
+     (let ((val (cadr ast)))
+       (cond ((exact-integer? val)
+              (number->string val))
+             (else
+              "unknown literal" ast))))
+    ((six.identifier)
+     (global-ref ast))
+    ((six.x+y six.x-y six.x*y)
+     (string-append "("
+                    (comp-rvalue ctx (cadr ast))
+                    (case (car ast)
+                      ((six.x+y) " + ")
+                      ((six.x-y) " - ")
+                      ((six.x*y) " * "))
+                    (comp-rvalue ctx (caddr ast))
+                    ")"))
+    (else
+     (error "unknown rvalue" ast))))
+
+(define (comp-program ast)
+  (let ((ctx (make-ctx '())))
+    (comp-glo-decl-list ctx ast)
+    (codegen ctx)))
+
+(define (codegen ctx)
+  (println runtime-prelude)
+  (for-each (lambda (decl)
+              (println decl))
+            (reverse (ctx-glo-decls ctx)))
+  (println runtime-postlude))
+
+(define runtime-prelude
+  (if support-addr-of?
+      (string-append
+       "defglo() { : $((ALLOC = ALLOC+1)) $(($1 = ALLOC)) $((_$ALLOC = $2)) ; }\n")
+      (string-append
+       "defglo() { : $(($1 = $2)) ; }\n")))
+
+(define runtime-postlude
+  (string-append
+   "printf \"%d\\n\" $(( " (global-ref '(six.identifier n)) " ))\n"))
+
+(define (comp-file path)
+  (let ((ast (call-with-input-file path read-six)))
+    (comp-program ast)))
+
 (define (read-six port)
   (##six-types-set! '((void . #f) (char . #f) (int . #f)))
   (let ((rt (input-port-readtable port)))
     (input-port-readtable-set! port (readtable-start-syntax-set rt 'six))
     (read-all port)))
 
-(define (comp-program ast)
-  (pp ast)) ;; TODO!
-
-(define (comp-file path)
-  (let ((ast (call-with-input-file path read-six)))
-    (comp-program ast)))
-
 (define (main . args)
   (for-each comp-file args))
 
 ;; Example:
 ;;
-;; $ cat test2.c
-;; int square(int x) {
-;;   return x*x;
-;; }
+;; $ cat test3.c
+;; int n = 6;
 ;; 
-;; void main()
-;; {
-;;   int n = square(10);
+;; n = n*7;
 ;; 
-;;   /* print a number to stdout */
+;; ---------- with: (define support-addr-of? #f)
+;; $ gsi six-cc.scm test3.c 
+;; defglo() { : $(($1 = $2)) ; }
 ;; 
-;;   int p = 1;
+;; defglo _n 6
+;; : $(( _n = (_n * 7) ))
+;; printf "%d\n" $(( _n ))
 ;; 
-;;   while (p * 10 <= n) p *= 10;
+;; $ gsi six-cc.scm test3.c | sh
+;; 42
 ;; 
-;;   while (p > 0) {
-;;     int digit = n / p;
-;;     putchar(48 + digit);
-;;     n %= p;
-;;     p /= 10;
-;;   }
+;; ---------- with: (define support-addr-of? #t)
+;; $ gsi six-cc.scm test3.c 
+;; defglo() { : $((ALLOC = ALLOC+1)) $(($1 = ALLOC)) $((_$ALLOC = $2)) ; }
 ;; 
-;;   putchar(10);
-;; }
-;; $ gsi six-cc.scm test2.c 
-;; ((six.define-procedure
-;;   (six.identifier square)
-;;   (six.procedure
-;;    int
-;;    (((six.identifier x) int))
-;;    (six.procedure-body
-;;     (six.return (six.x*y (six.identifier x) (six.identifier x))))))
-;;  (six.define-procedure
-;;   (six.identifier main)
-;;   (six.procedure
-;;    void
-;;    ()
-;;    (six.procedure-body
-;;     (six.define-variable
-;;      (six.identifier n)
-;;      int
-;;      ()
-;;      (six.call (six.identifier square) (six.literal 10)))
-;;     (six.define-variable (six.identifier p) int () (six.literal 1))
-;;     (six.while
-;;      (six.x<=y
-;;       (six.x*y (six.identifier p) (six.literal 10))
-;;       (six.identifier n))
-;;      (six.x*=y (six.identifier p) (six.literal 10)))
-;;     (six.while
-;;      (six.x>y (six.identifier p) (six.literal 0))
-;;      (six.compound
-;;       (six.define-variable
-;;        (six.identifier digit)
-;;        int
-;;        ()
-;;        (six.x/y (six.identifier n) (six.identifier p)))
-;;       (six.call
-;;        (six.identifier putchar)
-;;        (six.x+y (six.literal 48) (six.identifier digit)))
-;;       (six.x%=y (six.identifier n) (six.identifier p))
-;;       (six.x/=y (six.identifier p) (six.literal 10))))
-;;     (six.call (six.identifier putchar) (six.literal 10))))))
+;; defglo _n 6
+;; : $(( _$_n = (_$_n * 7) ))
+;; printf "%d\n" $(( _$_n ))
+;; 
+;; $ gsi six-cc.scm test3.c | sh
+;; 42
