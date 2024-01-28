de0156bb5a06962cab1e22b3f2d832bf05287b2c
Laurent Huberdeau <laurenthuberd@gmail.com>
2024-01-28 15:17:04 -0500

    Fix indentation of winter-pi2.c

--- winter-pi2.c
+++ winter-pi2.c
@@ -12,34 +12,33 @@ int c = 0;
 
 int main() {
 
-    for (; i < 2800; i++) {
-	r[i] = 2000;
+  for (; i < 2800; i++) {
+    r[i] = 2000;
+  }
+  r[i] = 0;
+
+  for (k = 2800; k > 0; k = k - 14) {
+
+    d = 0;
+    i = k;
+    for (;;) {
+      d = d + r[i] * 10000;
+      b = 2 * i - 1;
+
+      r[i] = d % b;
+      d = d / b;
+      i--;
+      if (i == 0) break;
+      d = d * i;
     }
-    r[i] = 0;
+    putchar(48 + (c + d / 10000) / 1000 % 10);
+    putchar(48 + (c + d / 10000) / 100 % 10);
+    putchar(48 + (c + d / 10000) / 10 % 10);
+    putchar(48 + (c + d / 10000) % 10);
+    c = d % 10000;
+  }
 
-    for (k = 2800; k > 0; k = k - 14) {
+  putchar(10);
 
-	d = 0;
-
-	i = k;
-	for (;;) {
-	    d = d + r[i] * 10000;
-	    b = 2 * i - 1;
-
-	    r[i] = d % b;
-	    d = d / b;
-	    i--;
-	    if (i == 0) break;
-	    d = d * i;
-	}
-        putchar(48 + (c + d / 10000) / 1000 % 10);
-        putchar(48 + (c + d / 10000) / 100 % 10);
-        putchar(48 + (c + d / 10000) / 10 % 10);
-        putchar(48 + (c + d / 10000) % 10);
-	c = d % 10000;
-    }
-
-    putchar(10);
-
-    return 0;
+  return 0;
 }
