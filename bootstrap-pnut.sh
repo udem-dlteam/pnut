de982e66bbe49986c7ce33ad6aab59395fd601f2
Laurent Huberdeau <laurenthuberd@gmail.com>
2024-04-13 13:43:10 -0400

    Allow use of other shells in bootstrap-pnut.sh

--- bootstrap-pnut.sh
+++ bootstrap-pnut.sh
@@ -1,13 +1,38 @@
 #! /bin/sh
 
-gcc -o pnut.exe pnut.c
+if [ -z $SHELL ]; then
+  echo "SHELL not set"
+  exit 1
+fi
 
-gcc -E -C -P -DPNUT_CC pnut.c > pnut-after-cpp.c
+bootstrap_with_shell() {
 
-./pnut.exe < pnut-after-cpp.c > pnut.sh
+  gcc -o pnut.exe pnut.c
 
-time ksh pnut.sh --no-zero-globals < pnut-after-cpp.c > pnut-twice-bootstrapped.sh
+  gcc -E -C -P -DPNUT_CC pnut.c > pnut-after-cpp.c
 
-diff pnut.sh pnut-twice-bootstrapped.sh
+  ./pnut.exe < pnut-after-cpp.c > pnut.sh
 
-wc pnut.c pnut.sh pnut-twice-bootstrapped.sh
+  echo "Bootstrap with $1"
+
+  time $1 pnut.sh --no-zero-globals < pnut-after-cpp.c > pnut-twice-bootstrapped.sh
+
+  diff pnut.sh pnut-twice-bootstrapped.sh
+
+  wc pnut.c pnut.sh pnut-twice-bootstrapped.sh
+}
+
+# Handle runtime options
+TEST_ALL_SHELLS=0
+
+if [ $# -gt 0 ] && [ $1 = "TEST_ALL_SHELLS" ] ; then TEST_ALL_SHELLS=1; shift; fi
+
+bootstrap_with_shell "ksh"
+
+if [ $TEST_ALL_SHELLS -ne 0 ]; then
+  bootstrap_with_shell "dash"
+  bootstrap_with_shell "bash"
+  bootstrap_with_shell "zsh"
+  bootstrap_with_shell "yash"
+  bootstrap_with_shell "mksh"
+fi
