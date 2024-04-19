1fc22e452160d4fcb39aed5029c73defda62415b
Laurent Huberdeau <laurenthuberd@gmail.com>
2024-04-19 13:37:50 -0400

    Add boostrap-pnut files to gitignore

--- bootstrap-pnut-x86.sh
+++ bootstrap-pnut-x86.sh
@@ -1,9 +1,8 @@
 #! /bin/sh
 
-if [ -z $SHELL ]; then
-  echo "SHELL not set"
-  exit 1
-fi
+TEMP_DIR="bootstrap-results"
+
+if [ ! -d "$TEMP_DIR" ]; then mkdir "$TEMP_DIR"; fi
 
 printf_timing() {
   msg=$1
@@ -18,31 +17,35 @@ bootstrap_with_shell() {
 
   # create pnut-sh.sh, the C to shell compiler as a shell script
 
-  gcc -o pnut-sh-compiled-by-gcc.exe -Dsh pnut.c
-  gcc -E -P -DPNUT_CC -Dsh pnut.c > pnut-sh-after-cpp.c
-  ./pnut-sh-compiled-by-gcc.exe < pnut-sh-after-cpp.c > pnut-sh.sh
+  gcc -o $TEMP_DIR/pnut-sh-compiled-by-gcc.exe -Dsh pnut.c
+  gcc -E -P -DPNUT_CC -Dsh pnut.c > "$TEMP_DIR/pnut-sh-after-cpp.c"
+  ./$TEMP_DIR/pnut-sh-compiled-by-gcc.exe < "$TEMP_DIR/pnut-sh-after-cpp.c" > $TEMP_DIR/pnut-sh.sh
 
   # create pnut-i386.sh, the C to i386 machine code compiler as a shell script
 
-  gcc -E -P -DPNUT_CC -Di386 pnut.c > pnut-i386-after-cpp.c
-  printf_timing "pnut-sh.sh compiling pnut.c -> pnut-sh-compiled-by-pnut-sh-sh.sh" "$1 pnut-sh.sh --no-zero-globals < pnut-sh-after-cpp.c > pnut-sh-compiled-by-pnut-sh-sh.sh"
-  if diff pnut-sh.sh pnut-sh-compiled-by-pnut-sh-sh.sh 2>&1 > /dev/null ; then
+  gcc -E -P -DPNUT_CC -Di386 pnut.c > $TEMP_DIR/pnut-i386-after-cpp.c
+  printf_timing "pnut-sh.sh compiling pnut.c -> pnut-sh-compiled-by-pnut-sh-sh.sh" \
+                "$1 $TEMP_DIR/pnut-sh.sh --no-zero-globals < $TEMP_DIR/pnut-sh-after-cpp.c > $TEMP_DIR/pnut-sh-compiled-by-pnut-sh-sh.sh"
+  if diff $TEMP_DIR/pnut-sh.sh $TEMP_DIR/pnut-sh-compiled-by-pnut-sh-sh.sh 2>&1 > /dev/null ; then
     printf "         SUCCESS... %s\n" "pnut-sh.sh == pnut-sh-compiled-by-pnut-sh-sh.sh"
   else
     printf "         FAILURE... %s\n" "pnut-sh.sh != pnut-sh-compiled-by-pnut-sh-sh.sh"
     exit 1
   fi
-  printf_timing "pnut-sh.sh compiling pnut.c -> pnut-i386-compiled-by-pnut-sh-sh.sh" "$1 pnut-sh.sh --no-zero-globals < pnut-i386-after-cpp.c > pnut-i386-compiled-by-pnut-sh-sh.sh"
-  printf_timing "pnut-i386-compiled-by-pnut-sh-sh.sh compiling pnut.c -> pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe" "$1 pnut-i386-compiled-by-pnut-sh-sh.sh --no-zero-globals < pnut-i386-after-cpp.c > pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe"
+  printf_timing "pnut-sh.sh compiling pnut.c -> pnut-i386-compiled-by-pnut-sh-sh.sh" \
+                "$1 $TEMP_DIR/pnut-sh.sh --no-zero-globals < $TEMP_DIR/pnut-i386-after-cpp.c > $TEMP_DIR/pnut-i386-compiled-by-pnut-sh-sh.sh"
+  printf_timing "pnut-i386-compiled-by-pnut-sh-sh.sh compiling pnut.c -> pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe" \
+                "$1 $TEMP_DIR/pnut-i386-compiled-by-pnut-sh-sh.sh --no-zero-globals < $TEMP_DIR/pnut-i386-after-cpp.c > $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe"
 
-  chmod +x pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe
+  chmod +x $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe
 
-  printf_timing "pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe compiling pnut.c -> pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe" "./pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe < pnut-i386-after-cpp.c > pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe"
+  printf_timing "pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe compiling pnut.c -> pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe" \
+                "./$TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe < $TEMP_DIR/pnut-i386-after-cpp.c > $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe"
 
-  chmod +x pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe
+  chmod +x $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe
 
-  if [ -s pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe ] ; then
-    if diff pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe 2>&1 > /dev/null ; then
+  if [ -s $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe ] ; then
+    if diff $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe 2>&1 > /dev/null ; then
       printf "         SUCCESS... %s\n" "pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe == pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe"
     else
       printf "         FAILURE... %s\n" "pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe != pnut-sh-compiled-by-pnut-sh-sh.sh"
@@ -58,27 +61,27 @@ bootstrap_with_shell() {
     printf "\n"
 
     ls -l \
-    pnut-sh.sh \
-    pnut-sh-compiled-by-pnut-sh-sh.sh
+    "$TEMP_DIR/pnut-sh.sh" \
+    "$TEMP_DIR/pnut-sh-compiled-by-pnut-sh-sh.sh"
 
     printf "\n"
 
     sha1sum \
-    pnut-sh.sh \
-    pnut-sh-compiled-by-pnut-sh-sh.sh
+    "$TEMP_DIR/pnut-sh.sh" \
+    "$TEMP_DIR/pnut-sh-compiled-by-pnut-sh-sh.sh"
 
     printf "\n"
 
     ls -l \
-    pnut-sh-compiled-by-gcc.exe \
-    pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe \
-    pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe
+    "$TEMP_DIR/pnut-sh-compiled-by-gcc.exe" \
+    "$TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe" \
+    "$TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe"
 
     printf "\n"
 
     sha1sum \
-    pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe \
-    pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe
+    "$TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe" \
+    "$TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe"
 
   fi
 }
--- bootstrap-pnut.sh
+++ bootstrap-pnut.sh
@@ -1,25 +1,24 @@
 #! /bin/sh
 
-if [ -z $SHELL ]; then
-  echo "SHELL not set"
-  exit 1
-fi
+TEMP_DIR="bootstrap-results"
+
+if [ ! -d "$TEMP_DIR" ]; then mkdir "$TEMP_DIR"; fi
+
+gcc -o "$TEMP_DIR/pnut.exe" -Dsh pnut.c
+
+gcc -E -C -P -DPNUT_CC -Dsh pnut.c > "$TEMP_DIR/pnut-after-cpp.c"
+
+./$TEMP_DIR/pnut.exe < "$TEMP_DIR/pnut-after-cpp.c" > "$TEMP_DIR/pnut.sh"
 
 bootstrap_with_shell() {
 
-  gcc -o pnut.exe -Dsh pnut.c
-
-  gcc -E -C -P -DPNUT_CC -Dsh pnut.c > pnut-after-cpp.c
-
-  ./pnut.exe < pnut-after-cpp.c > pnut.sh
-
   echo "Bootstrap with $1"
 
-  time $1 pnut.sh --no-zero-globals < pnut-after-cpp.c > pnut-twice-bootstrapped.sh
+  time $1 "$TEMP_DIR/pnut.sh" --no-zero-globals < "$TEMP_DIR/pnut-after-cpp.c" > "$TEMP_DIR/pnut-twice-bootstrapped.sh"
 
-  diff pnut.sh pnut-twice-bootstrapped.sh
+  diff "$TEMP_DIR/pnut.sh" "$TEMP_DIR/pnut-twice-bootstrapped.sh"
 
-  wc pnut.c pnut.sh pnut-twice-bootstrapped.sh
+  wc pnut.c "$TEMP_DIR/pnut.sh" "$TEMP_DIR/pnut-twice-bootstrapped.sh"
 }
 
 # Handle runtime options
