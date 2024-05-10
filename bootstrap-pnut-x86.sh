a3cb3db63637b000895e6e6735c27b564ed47bf7
Laurent Huberdeau <laurenthuberd@gmail.com>
2024-05-10 14:12:22 -0400

    Boostrap x86 pnut using GCC instead of shell
    Bootstrapping using the shell took too long and
    wasn't necessary in most cases. The
    bootstrap-pnut-x86.sh script now defaults to
    bootstrapping using GCC. The shell boostrap can be
    tested passing the `WITH_SHELL` parameter.

--- bootstrap-pnut-x86.sh
+++ bootstrap-pnut-x86.sh
@@ -2,6 +2,8 @@
 
 TEMP_DIR="bootstrap-results"
 
+PNUT_x86_OPTIONS="-Di386"
+
 if [ ! -d "$TEMP_DIR" ]; then mkdir "$TEMP_DIR"; fi
 
 printf_timing() {
@@ -9,6 +11,33 @@ printf_timing() {
   cmd=$2
   real_time=`/usr/bin/time -p sh -c "$cmd" 2>&1 | grep '^real ' | sed 's/.* //'`
   printf "%ss %s\n" $real_time "$msg"
+}
+
+bootstrap_with_gcc() {
+
+  echo "===================== Bootstrap with GCC"
+
+  # Compile pnut x86 using GCC, then compile pnut x86 using pnut x86
+
+  gcc -o $TEMP_DIR/pnut-x86-by-gcc.exe $PNUT_x86_OPTIONS pnut.c
+  gcc -E -P -DPNUT_CC $PNUT_x86_OPTIONS pnut.c > "$TEMP_DIR/pnut-after-cpp.c"
+  ./$TEMP_DIR/pnut-x86-by-gcc.exe < "$TEMP_DIR/pnut-after-cpp.c" > $TEMP_DIR/pnut-x86-by-pnut-x86-by-gcc.exe
+
+  chmod +x $TEMP_DIR/pnut-x86-by-pnut-x86-by-gcc.exe
+
+  ./$TEMP_DIR/pnut-x86-by-pnut-x86-by-gcc.exe < "$TEMP_DIR/pnut-after-cpp.c" > $TEMP_DIR/pnut-x86-by-pnut-x86-by-pnut-x86-by-gcc.exe
+
+  if [ -s $TEMP_DIR/pnut-x86-by-pnut-x86-by-pnut-x86-by-gcc.exe ] ; then
+    if diff $TEMP_DIR/pnut-x86-by-pnut-x86-by-gcc.exe $TEMP_DIR/pnut-x86-by-pnut-x86-by-pnut-x86-by-gcc.exe 2>&1 > /dev/null ; then
+      printf "         SUCCESS... %s\n" "pnut-x86-by-pnut-x86-by-gcc.exe == pnut-x86-by-pnut-x86-by-pnut-x86-by-gcc.exe"
+    else
+      printf "         FAILURE... %s\n" "pnut-x86-by-pnut-x86-by-gcc.exe != pnut-x86-by-pnut-x86-by-pnut-x86-by-gcc.exe"
+      exit 1
+    fi
+  else
+    printf "         FAILURE... %s\n" "pnut-x86-by-pnut-x86-by-pnut-x86-by-gcc.exe is empty! (compiler crash?)"
+    exit 1
+  fi
 }
 
 bootstrap_with_shell() {
@@ -23,7 +52,7 @@ bootstrap_with_shell() {
 
   # create pnut-i386.sh, the C to i386 machine code compiler as a shell script
 
-  gcc -E -P -DPNUT_CC -Di386 pnut.c > $TEMP_DIR/pnut-i386-after-cpp.c
+  gcc -E -P -DPNUT_CC $PNUT_x86_OPTIONS pnut.c > $TEMP_DIR/pnut-i386-after-cpp.c
   printf_timing "pnut-sh.sh compiling pnut.c -> pnut-sh-compiled-by-pnut-sh-sh.sh" \
                 "$1 $TEMP_DIR/pnut-sh.sh --no-zero-globals < $TEMP_DIR/pnut-sh-after-cpp.c > $TEMP_DIR/pnut-sh-compiled-by-pnut-sh-sh.sh"
   if diff $TEMP_DIR/pnut-sh.sh $TEMP_DIR/pnut-sh-compiled-by-pnut-sh-sh.sh 2>&1 > /dev/null ; then
@@ -87,11 +116,17 @@ bootstrap_with_shell() {
 }
 
 # Handle runtime options
+WITH_SHELL=0
 TEST_ALL_SHELLS=0
 
+if [ $# -gt 0 ] && [ $1 = "WITH_SHELL" ] ; then WITH_SHELL=1; shift; fi
 if [ $# -gt 0 ] && [ $1 = "TEST_ALL_SHELLS" ] ; then TEST_ALL_SHELLS=1; shift; fi
 
-bootstrap_with_shell "ksh"
+if [ $WITH_SHELL -ne 0 ]; then
+  bootstrap_with_shell "ksh"
+else
+  bootstrap_with_gcc # Much faster than bootstrapping on the Shell
+fi
 
 if [ $TEST_ALL_SHELLS -ne 0 ]; then
   bootstrap_with_shell "dash"
