fc701851adf00f804514d8dc31dcc64238b88971
Kraft-Single <cassandre.hamel.1@umontreal.ca>
2024-05-15 18:27:12 -0400

    [Tests changed to make them compatible with pnut] NOTE: Not all tests are still currently compatible this applies only to select tests in six-cc-tests directory NOTE 2: Tests getchar need to be reverted in the future as they were changed for local script testing purposes bug 1 : There is an issue with break causing unexpected behavior this is further detailed in the tests while-continue.c and while-fun-call.c

--- six-cc-tests/conditions.c
+++ six-cc-tests/conditions.c
@@ -1,15 +1,28 @@
+void putstring(char_ptr s) {
+  while (*s) {
+    putchar(*s);
+    s = s + 1;
+  }
+}
+
 void main() {
   int i = 0;
   int j = 5;
   int k = 12;
-  char_ptr p = "_hello";
+  char * p = "_hello";
 
   if ((*p >= 'a' && *p <= 'z') || (*p >= 'A' && *p <= 'Z') || (*p >= '0' && *p <= '9') || *p == '_')
   {
-    printf("p: %c\n", *p);
+    //printf("p: %c\n", *p);
+    putstring("p: ");
+    putchar(*p);
+    putchar('\n');
   }
   if ((*p >= 'a' || *p <= 'z') && (*p >= 'A' || *p <= 'Z') && !(*p >= '0' || *p <= '9') && *p == '_')
   {
-    printf("p: %c\n", *p);
+    //printf("p: %c\n", *p);
+    putstring("p: ");
+    putchar(*p);
+    putchar('\n');
   }
 }
--- six-cc-tests/init.c
+++ six-cc-tests/init.c
@@ -1,10 +1,29 @@
+void putstring(char *s) {
+  while (*s) {
+    putchar(*s);
+    s = s + 1;
+  }
+}
+
 void main() {
   int i = 0;
   int j = 5;
   int k;
-  char_ptr s;
-  char_ptr t = "Hello, world!";
+  char * s;
+  char * t = "Hello, world!";
   k = 42;
-  printf("i: %d, j: %d, k: %d\n", i, j, k);
-  printf("t: %s\n", t);
+//  printf("i: %d, j: %d, k: %d\n", i, j, k);
+//  printf("t: %s\n", t);
+  putstring("i: ");
+  putchar(i + 48);
+  putstring(", j: ");
+  putchar(j + 48);
+  putstring(", k: ");
+  putchar(k + 48);
+  putchar('\n');
+  putstring("t: ");
+  putstring(t);
+  putchar('\n');
+
+  return 0;
 }
