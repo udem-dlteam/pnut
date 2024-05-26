182f5fe7723022ab5886c9f48a936661751f2c3c
Laurent Huberdeau <laurenthuberd@gmail.com>
2024-05-26 00:16:42 -0400

    Bootstrap: Read file with fopen instead of getchar

--- bootstrap-pnut-x86.sh
+++ bootstrap-pnut-x86.sh
@@ -2,7 +2,9 @@
 
 TEMP_DIR="bootstrap-results"
 
-PNUT_x86_OPTIONS="-Dx86_64"
+# PNUT_x86_OPTIONS="-DSUPPORT_INCLUDE -Dx86_64"
+PNUT_x86_OPTIONS="-DSUPPORT_INCLUDE -Di386"
+PNUT_SH_OPTIONS="-DSUPPORT_INCLUDE -Dsh"
 
 if [ ! -d "$TEMP_DIR" ]; then mkdir "$TEMP_DIR"; fi
 
@@ -21,11 +23,11 @@ bootstrap_with_gcc() {
 
   gcc -o $TEMP_DIR/pnut-x86-by-gcc.exe $PNUT_x86_OPTIONS pnut.c
   gcc -E -P -DPNUT_CC $PNUT_x86_OPTIONS pnut.c > "$TEMP_DIR/pnut-after-cpp.c"
-  ./$TEMP_DIR/pnut-x86-by-gcc.exe < "$TEMP_DIR/pnut-after-cpp.c" > $TEMP_DIR/pnut-x86-by-pnut-x86-by-gcc.exe
+  ./$TEMP_DIR/pnut-x86-by-gcc.exe "$TEMP_DIR/pnut-after-cpp.c" > $TEMP_DIR/pnut-x86-by-pnut-x86-by-gcc.exe
 
   chmod +x $TEMP_DIR/pnut-x86-by-pnut-x86-by-gcc.exe
 
-  ./$TEMP_DIR/pnut-x86-by-pnut-x86-by-gcc.exe < "$TEMP_DIR/pnut-after-cpp.c" > $TEMP_DIR/pnut-x86-by-pnut-x86-by-pnut-x86-by-gcc.exe
+  ./$TEMP_DIR/pnut-x86-by-pnut-x86-by-gcc.exe "$TEMP_DIR/pnut-after-cpp.c" > $TEMP_DIR/pnut-x86-by-pnut-x86-by-pnut-x86-by-gcc.exe
 
   if [ -s $TEMP_DIR/pnut-x86-by-pnut-x86-by-pnut-x86-by-gcc.exe ] ; then
     if diff $TEMP_DIR/pnut-x86-by-pnut-x86-by-gcc.exe $TEMP_DIR/pnut-x86-by-pnut-x86-by-pnut-x86-by-gcc.exe 2>&1 > /dev/null ; then
@@ -46,15 +48,15 @@ bootstrap_with_shell() {
 
   # create pnut-sh.sh, the C to shell compiler as a shell script
 
-  gcc -o $TEMP_DIR/pnut-sh-compiled-by-gcc.exe -Dsh pnut.c
-  gcc -E -P -DPNUT_CC -Dsh pnut.c > "$TEMP_DIR/pnut-sh-after-cpp.c"
-  ./$TEMP_DIR/pnut-sh-compiled-by-gcc.exe < "$TEMP_DIR/pnut-sh-after-cpp.c" > $TEMP_DIR/pnut-sh.sh
+  gcc -o $TEMP_DIR/pnut-sh-compiled-by-gcc.exe $PNUT_SH_OPTIONS pnut.c
+  gcc -E -P -DPNUT_CC $PNUT_SH_OPTIONS pnut.c > "$TEMP_DIR/pnut-sh-after-cpp.c"
+  ./$TEMP_DIR/pnut-sh-compiled-by-gcc.exe "$TEMP_DIR/pnut-sh-after-cpp.c" > $TEMP_DIR/pnut-sh.sh
 
   # create pnut-i386.sh, the C to i386 machine code compiler as a shell script
 
   gcc -E -P -DPNUT_CC $PNUT_x86_OPTIONS pnut.c > $TEMP_DIR/pnut-i386-after-cpp.c
   printf_timing "pnut-sh.sh compiling pnut.c -> pnut-sh-compiled-by-pnut-sh-sh.sh" \
-                "$1 $TEMP_DIR/pnut-sh.sh --no-zero-globals < $TEMP_DIR/pnut-sh-after-cpp.c > $TEMP_DIR/pnut-sh-compiled-by-pnut-sh-sh.sh"
+                "$1 $TEMP_DIR/pnut-sh.sh --no-zero-globals $TEMP_DIR/pnut-sh-after-cpp.c > $TEMP_DIR/pnut-sh-compiled-by-pnut-sh-sh.sh"
   if diff $TEMP_DIR/pnut-sh.sh $TEMP_DIR/pnut-sh-compiled-by-pnut-sh-sh.sh 2>&1 > /dev/null ; then
     printf "         SUCCESS... %s\n" "pnut-sh.sh == pnut-sh-compiled-by-pnut-sh-sh.sh"
   else
@@ -62,14 +64,14 @@ bootstrap_with_shell() {
     exit 1
   fi
   printf_timing "pnut-sh.sh compiling pnut.c -> pnut-i386-compiled-by-pnut-sh-sh.sh" \
-                "$1 $TEMP_DIR/pnut-sh.sh --no-zero-globals < $TEMP_DIR/pnut-i386-after-cpp.c > $TEMP_DIR/pnut-i386-compiled-by-pnut-sh-sh.sh"
+                "$1 $TEMP_DIR/pnut-sh.sh --no-zero-globals $TEMP_DIR/pnut-i386-after-cpp.c > $TEMP_DIR/pnut-i386-compiled-by-pnut-sh-sh.sh"
   printf_timing "pnut-i386-compiled-by-pnut-sh-sh.sh compiling pnut.c -> pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe" \
-                "$1 $TEMP_DIR/pnut-i386-compiled-by-pnut-sh-sh.sh --no-zero-globals < $TEMP_DIR/pnut-i386-after-cpp.c > $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe"
+                "$1 $TEMP_DIR/pnut-i386-compiled-by-pnut-sh-sh.sh --no-zero-globals $TEMP_DIR/pnut-i386-after-cpp.c > $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe"
 
   chmod +x $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe
 
   printf_timing "pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe compiling pnut.c -> pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe" \
-                "./$TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe < $TEMP_DIR/pnut-i386-after-cpp.c > $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe"
+                "./$TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe $TEMP_DIR/pnut-i386-after-cpp.c > $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe"
 
   chmod +x $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe
 
