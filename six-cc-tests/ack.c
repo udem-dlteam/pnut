e95c59d54e3d1b4bde4b39f73a72c2109109d3ce
Kraft-Single <cassandre.hamel.1@umontreal.ca>
2024-06-07 11:26:18 -0400

    Added tests and script for pnut
        added pre-post inc/dec tests
        added enum and malloc-free tests
        added variable decl tests
        added WIP run-test script
    
    [Six-cc-tests changed to make them compatible with pnut]
    NOTE: Not all tests are still currently compatible this applies only to select tests in six-cc-tests directory
    NOTE 2: Tests getchar need to be reverted in the future as they were changed for local script testing purposes
    
    parent a1ee4e711319aa990aacec60d4190c7f07d0036d
    author Kraft-Single <cassandre.hamel.1@umontreal.ca> 1717773978 -0400
    committer Kraft-Single <cassandre.hamel.1@umontreal.ca> 1718391539 -0400

--- six-cc-tests/ack.c
+++ six-cc-tests/ack.c
@@ -1,3 +1,10 @@
+void putstring(char *s) {
+  while (*s) {
+    putchar(*s);
+    s = s + 1;
+  }
+}
+
 int ack(int m, int n)
 {
   int a; /* Local variable so that the function is not simple */
@@ -9,5 +16,14 @@ int ack(int m, int n)
 int main() {
   int m = 3;
   int n = 3;
-  printf("ack(%d, %d) = %d", m, n, ack(m, n));
+  //printf("ack(%d, %d) = %d", m, n, ack(m, n));
+  //replace the printf with putstring
+  putstring("ack(");
+  putchar(48 + m);
+  putstring(", ");
+  putchar(48 + n);
+  putstring(") = ");
+  int ack = ack(m, n);
+  putchar(ack); //'=' == 61
+  return 0;
 }
