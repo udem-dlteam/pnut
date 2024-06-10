2d623865eba1f4a35c1d9cba6261488cb0b67bc7
Kraft-Single <cassandre.hamel.1@umontreal.ca>
2024-06-10 16:33:20 -0400

    fixed some broken tests

--- tests/test.c
+++ tests/test.c
@@ -1,16 +1,19 @@
-#include <stdio.h>
+//#include <stdio.h>
 
-#define NL 10
-#define ZERO '0'
+//#define NL 10
+//#define ZERO '0'
 
 void main() {
 
   /* print a number to stdout */
-
+  int NL = 10;
+  int ZERO = '0';
   int n = 31416;
   int p = 1;
 
-  while (p * 10 <= n) p *= 10;
+  while (p * 10 <= n){
+    p *= 10;
+  }
 
   while (p > 0) {
     int digit = n / p;
