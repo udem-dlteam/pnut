b059cb2d21d649bb66aeda910b218e99ba34e647
Laurent Huberdeau <laurenthuberd@gmail.com>
2024-06-16 09:52:35 +0200

    Generate shell runtime from C code
    The generated runtime matches the original runtime.sh byte for byte. The
    make-runtime-sh.c program can be used to generate the runtime.sh file.
    
      > gcc -o make-runtime-sh.exe make-runtime-sh.c
      > ./make-runtime-sh.exe > runtime-generated.sh
      > diff runtime-generated.sh runtime.sh

---
+++ make-runtime-sh.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef int bool;
+#define true 1
+#define false 0
+
+void putstr(char* s) {
+  while (*s) putchar(*s++);
+}
+
+// Runtime configuration
+#define RT_FREE_UNSETS_VARS
+#define RT_INIT_GLOBALS
+
+#include "sh-runtime.c"
+
+void main() {
+  produce_runtime();
+}
