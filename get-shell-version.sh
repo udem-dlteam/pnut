b75c18be7e20b7f7f6456186159d5a337fc48499
Kraft-Single <cassandre.hamel.1@umontreal.ca>
2024-06-26 13:05:40 -0400

    Reverted get shell version to just output without latex

--- get-shell-version.sh
+++ get-shell-version.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
 
 shell=$1
-output_file="shell_version_results.tex"
-
-# Initialize the LaTeX document if it does not exist
-if [ ! -f "$output_file" ]; then
-  echo "\\documentclass{article}" > $output_file
-  echo "\\usepackage{geometry}" >> $output_file
-  echo "\\geometry{a4paper, margin=1in}" >> $output_file
-  echo "\\begin{document}" >> $output_file
-  echo "\\begin{table}[h!]" >> $output_file
-  echo "\\centering" >> $output_file
-  echo "\\begin{tabular}{|c|c|}" >> $output_file
-  echo "\\hline" >> $output_file
-  echo "Shell & Version \\\\" >> $output_file
-  echo "\\hline" >> $output_file
-fi
-
-# Function to escape LaTeX special characters
-escape_latex() {
-  echo "$1" | sed -e 's/_/\\_/g' -e 's/%/\\%/g' -e 's/&/\\&/g' -e 's/#/\\#/g' -e 's/\$/\\\$/g' -e 's/{/\\{/g' -e 's/}/\\}/g' -e 's/\~/\\~/g' -e 's/\^/\\^/g'
-}
 
 # Function to get shell version
 get_shell_version() {
@@ -57,21 +37,8 @@ get_shell_version() {
 
 # Check if shell is installed
 if ! command -v $shell &> /dev/null; then
-  version="$shell not found"
+  echo "$shell not found"
 else
   version=$(get_shell_version)
-  version=$(escape_latex "$version")
-fi
-
-# Append results to the LaTeX table
-echo "$shell & $version \\\\" >> $output_file
-echo "\\hline" >> $output_file
-
-# Finalize the LaTeX table and document if this is the last shell
-if [ "$shell" == "yash" ]; then
-  echo "\\end{tabular}" >> $output_file
-  echo "\\caption{Shell Versions}" >> $output_file
-  echo "\\label{table:shell_versions}" >> $output_file
-  echo "\\end{table}" >> $output_file
-  echo "\\end{document}" >> $output_file
+  echo "$shell version: $version"
 fi
