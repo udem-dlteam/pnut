edd1f46c766aac97cf18417d259d548222254b87
Laurent Huberdeau <laurenthuberd@gmail.com>
2024-07-01 15:03:36 +0200

    Add special characters to char-to-int conv benchmark

--- benchmarks/char-to-int-conversion/lookup_table.sh
+++ benchmarks/char-to-int-conversion/lookup_table.sh
@@ -79,7 +79,6 @@ decode_chars() {
         ;;
       # Match on all other chars
       *)
-        echo "Wrong char"; exit 1;
         char_to_int_code=$(LC_CTYPE=C printf "%d" "'$1")
         ;;
     esac
@@ -104,6 +103,6 @@ done
 
 i=0
 while [ $i -lt $iterations ]; do
-  decode_chars "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" "0" "1" "2" "3" "4" "5" "6" "7" "8" "9"
+  decode_chars "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" " " "!" "#" "$" "%" "&" "(" ")" "*" "+" "," "-" "." "/" ":" ";" "<" "=" ">" "?" "@" "[" "]" "^" "_" "`" "{" "|" "}" "~"
   i=$((i+1))
 done
