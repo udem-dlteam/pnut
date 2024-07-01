b00dd01fb6720ee0310dc879af33d28921f9ba3e
Laurent Huberdeau <laurenthuberd@gmail.com>
2024-07-01 12:26:18 +0200

    Add pnut-exe to compile times benchmark

--- benchmarks/compile-times/run.sh
+++ benchmarks/compile-times/run.sh
@@ -16,9 +16,10 @@ compile() {
   # shell=$1
   file="examples/$2.c"
 
-  # set -x
   if [ $1 = "gcc" ]; then
     TIME_MS=$(( `bash -c "time $1 $COMP_DIR/pnut-sh-base.exe $file > $COMP_DIR/$2-with-$1.sh" 2>&1 | fgrep real | sed -e "s/real[^0-9]*//g" -e "s/m/*60000+/g" -e "s/s//g" -e "s/\\+0\\./-1000+1/g" -e "s/\\.//g"` ))
+  elif [ $1 = "pnut" ]; then
+    TIME_MS=$(( `bash -c "time $1 $COMP_DIR/pnut-sh-compiled-by-pnut-exe.exe $file > $COMP_DIR/$2-with-$1.sh" 2>&1 | fgrep real | sed -e "s/real[^0-9]*//g" -e "s/m/*60000+/g" -e "s/s//g" -e "s/\\+0\\./-1000+1/g" -e "s/\\.//g"` ))
   else
     TIME_MS=$(( `bash -c "time $1 $COMP_DIR/pnut.sh $file > $COMP_DIR/$2-with-$1.sh" 2>&1 | fgrep real | sed -e "s/real[^0-9]*//g" -e "s/m/*60000+/g" -e "s/s//g" -e "s/\\+0\\./-1000+1/g" -e "s/\\.//g"` ))
   fi
@@ -26,13 +27,19 @@ compile() {
   # exit 1
 }
 
-PNUT_OPTIONS="-DSUPPORT_INCLUDE -DRT_NO_INIT_GLOBALS -Dsh -DSH_INCLUDE_C_CODE -DOPTIMIZE_LONG_LINES"
-gcc -o $COMP_DIR/pnut-sh-base.exe $PNUT_OPTIONS -O2 pnut.c
-./$COMP_DIR/pnut-sh-base.exe $PNUT_OPTIONS pnut.c > $COMP_DIR/pnut.sh
+PNUT_SH_OPTIONS="-DSUPPORT_INCLUDE -DRT_NO_INIT_GLOBALS -Dsh"
+PNUT_x86_OPTIONS="-DSUPPORT_INCLUDE -Di386"
+gcc -o $COMP_DIR/pnut-sh-base.exe $PNUT_SH_OPTIONS -Os pnut.c
+gcc -o $COMP_DIR/pnut-exe.exe $PNUT_x86_OPTIONS -Os pnut.c
+# set -x
+./$COMP_DIR/pnut-sh-base.exe $PNUT_SH_OPTIONS pnut.c > $COMP_DIR/pnut.sh
+./$COMP_DIR/pnut-exe.exe $PNUT_SH_OPTIONS pnut.c > $COMP_DIR/pnut-sh-compiled-by-pnut-exe.exe
 
-programs="empty hello fib sha256sum"
+chmod +x $COMP_DIR/pnut-sh-compiled-by-pnut-exe.exe
+
+programs="empty hello fib"
 # programs="sha256sum"
-shells="ksh dash bash yash zsh gcc"
+shells="ksh dash bash yash zsh gcc pnut"
 
 for prog in $programs; do
   for shell in $shells; do
