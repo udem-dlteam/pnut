fa005710efd608cf7af2d163715f393578806997
Laurent Huberdeau <laurenthuberd@gmail.com>
2024-07-01 20:33:32 +0200

    Use temp vars in examples/fib.c

--- examples/fib.c
+++ examples/fib.c
@@ -1,11 +1,8 @@
 int fib(int n) {
-  int t1, t2;
   if (n < 2) {
     return n;
   } else {
-    t1 = fib(n - 1);
-    t2 = fib(n - 2);
-    return t1 + t2;
+    return fib(n - 1) + fib(n - 2);
   }
 }
 
