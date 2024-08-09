1a1571747b37907a7d14a2f43dfd649738fb431d
Laurent Huberdeau <laurenthuberd@gmail.com>
2024-08-08 22:18:12 -0400

    Parallelize bootstrap tests in Github actions

--- bootstrap-pnut-exe.sh
+++ bootstrap-pnut-exe.sh
@@ -1,10 +1,12 @@
 #! /bin/sh
 
+set -e -u
+
 TEMP_DIR="bootstrap-results"
 
-# PNUT_x86_OPTIONS="-Dmac_os"
-# PNUT_x86_OPTIONS="-Dx86_64"
-PNUT_x86_OPTIONS="-Di386"
+# PNUT_EXE_OPTIONS="-Dmac_os"
+# PNUT_EXE_OPTIONS="-Dx86_64"
+PNUT_EXE_OPTIONS="-Di386"
 PNUT_SH_OPTIONS="-DRT_NO_INIT_GLOBALS -Dsh"
 
 if [ ! -d "$TEMP_DIR" ]; then mkdir "$TEMP_DIR"; fi
@@ -22,13 +24,13 @@ bootstrap_with_gcc() {
 
   # Compile pnut x86 using GCC, then compile pnut x86 using pnut x86
 
-  gcc -o $TEMP_DIR/pnut-x86-by-gcc.exe $PNUT_x86_OPTIONS pnut.c
-  # gcc -E -P -DPNUT_CC $PNUT_x86_OPTIONS pnut.c > "$TEMP_DIR/pnut-after-cpp.c"
-  ./$TEMP_DIR/pnut-x86-by-gcc.exe $PNUT_x86_OPTIONS pnut.c > $TEMP_DIR/pnut-x86-by-pnut-x86-by-gcc.exe
+  gcc -o $TEMP_DIR/pnut-x86-by-gcc.exe $PNUT_EXE_OPTIONS pnut.c
+  # gcc -E -P -DPNUT_CC $PNUT_EXE_OPTIONS pnut.c > "$TEMP_DIR/pnut-after-cpp.c"
+  ./$TEMP_DIR/pnut-x86-by-gcc.exe $PNUT_EXE_OPTIONS pnut.c > $TEMP_DIR/pnut-x86-by-pnut-x86-by-gcc.exe
 
   chmod +x $TEMP_DIR/pnut-x86-by-pnut-x86-by-gcc.exe
 
-  ./$TEMP_DIR/pnut-x86-by-pnut-x86-by-gcc.exe $PNUT_x86_OPTIONS pnut.c > $TEMP_DIR/pnut-x86-by-pnut-x86-by-pnut-x86-by-gcc.exe
+  ./$TEMP_DIR/pnut-x86-by-pnut-x86-by-gcc.exe $PNUT_EXE_OPTIONS pnut.c > $TEMP_DIR/pnut-x86-by-pnut-x86-by-pnut-x86-by-gcc.exe
 
   if [ -s $TEMP_DIR/pnut-x86-by-pnut-x86-by-pnut-x86-by-gcc.exe ] ; then
     if diff $TEMP_DIR/pnut-x86-by-pnut-x86-by-gcc.exe $TEMP_DIR/pnut-x86-by-pnut-x86-by-pnut-x86-by-gcc.exe 2>&1 > /dev/null ; then
@@ -53,7 +55,7 @@ bootstrap_with_shell() {
   ./$TEMP_DIR/pnut-sh-compiled-by-gcc.exe $PNUT_SH_OPTIONS pnut.c > $TEMP_DIR/pnut-sh.sh
 
   # create pnut-i386.sh, the C to i386 machine code compiler as a shell script
-  # gcc -E -P -DPNUT_CC $PNUT_x86_OPTIONS pnut.c > $TEMP_DIR/pnut-i386-after-cpp.c
+  # gcc -E -P -DPNUT_CC $PNUT_EXE_OPTIONS pnut.c > $TEMP_DIR/pnut-i386-after-cpp.c
   printf_timing "pnut-sh.sh compiling pnut.c -> pnut-sh-compiled-by-pnut-sh-sh.sh" \
                 "$1 $TEMP_DIR/pnut-sh.sh $PNUT_SH_OPTIONS pnut.c > $TEMP_DIR/pnut-sh-compiled-by-pnut-sh-sh.sh"
   if diff $TEMP_DIR/pnut-sh.sh $TEMP_DIR/pnut-sh-compiled-by-pnut-sh-sh.sh 2>&1 > /dev/null ; then
@@ -63,14 +65,14 @@ bootstrap_with_shell() {
     exit 1
   fi
   printf_timing "pnut-sh.sh compiling pnut.c -> pnut-i386-compiled-by-pnut-sh-sh.sh" \
-                "$1 $TEMP_DIR/pnut-sh.sh $PNUT_x86_OPTIONS pnut.c > $TEMP_DIR/pnut-i386-compiled-by-pnut-sh-sh.sh"
+                "$1 $TEMP_DIR/pnut-sh.sh $PNUT_EXE_OPTIONS pnut.c > $TEMP_DIR/pnut-i386-compiled-by-pnut-sh-sh.sh"
   printf_timing "pnut-i386-compiled-by-pnut-sh-sh.sh compiling pnut.c -> pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe" \
-                "$1 $TEMP_DIR/pnut-i386-compiled-by-pnut-sh-sh.sh $PNUT_x86_OPTIONS pnut.c > $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe"
+                "$1 $TEMP_DIR/pnut-i386-compiled-by-pnut-sh-sh.sh $PNUT_EXE_OPTIONS pnut.c > $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe"
 
   chmod +x $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe
 
   printf_timing "pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe compiling pnut.c -> pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe" \
-                "./$TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe $PNUT_x86_OPTIONS pnut.c > $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe"
+                "./$TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe $PNUT_EXE_OPTIONS pnut.c > $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe"
 
   chmod +x $TEMP_DIR/pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-i386-compiled-by-pnut-sh-sh.exe
 
@@ -116,23 +118,39 @@ bootstrap_with_shell() {
   fi
 }
 
-# Handle runtime options
-WITH_SHELL=0
-TEST_ALL_SHELLS=0
+# Parse the arguments
+backend="x86_64"  # Default to x86_64 Linux
+shell=            # Defined if doing the full bootstrap using pnut.sh on Posix shell. "all" to test with all shells (slow).
 
-if [ $# -gt 0 ] && [ $1 = "WITH_SHELL" ] ; then WITH_SHELL=1; shift; fi
-if [ $# -gt 0 ] && [ $1 = "TEST_ALL_SHELLS" ] ; then TEST_ALL_SHELLS=1; shift; fi
+while [ $# -gt 0 ]; do
+  case $1 in
+    --backend) backend="$2"; shift 2 ;;
+    --shell) shell="$2"; shift 2 ;;
+    *) echo "Unknown option: $1"; exit 1;;
+  esac
+done
 
-if [ $WITH_SHELL -ne 0 ]; then
-  bootstrap_with_shell "ksh"
+case $backend in
+  i386 | x86_64 | mac_os)
+    PNUT_EXE_OPTIONS="-D$backend" ;;
+  *)
+    echo "Unknown backend: $backend"
+    exit 1
+    ;;
+esac
+
+if [ -z "$shell" ]; then
+  bootstrap_with_gcc
 else
-  bootstrap_with_gcc # Much faster than bootstrapping on the Shell
-fi
-
-if [ $TEST_ALL_SHELLS -ne 0 ]; then
-  bootstrap_with_shell "dash"
-  bootstrap_with_shell "bash"
-  bootstrap_with_shell "zsh"
-  bootstrap_with_shell "yash"
-  bootstrap_with_shell "mksh"
+  if [ "$shell" = "all" ]; then
+    set +e # Don't exit on error because we want to test all shells.
+    bootstrap_with_shell "dash"
+    bootstrap_with_shell "ksh"
+    bootstrap_with_shell "bash"
+    bootstrap_with_shell "yash"
+    bootstrap_with_shell "mksh"
+    bootstrap_with_shell "zsh"
+  else
+    bootstrap_with_shell "$shell"
+  fi
 fi
