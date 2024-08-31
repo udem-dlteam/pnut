9e5143865ec36d3ede6bef1b7664271cf3851635
Laurent Huberdeau <laurenthuberd@gmail.com>
2024-08-31 12:17:10 -0400

    Add negative-zero.c test to document bug

---
+++ tests/_bug/negative-zero.c
@@ -0,0 +1,26 @@
+// On ksh, when assigning the negative value of a variable containing 0 (i.e.
+// -0) to a variable using arithmetic expansion, the variable is assigned the
+// string `-0` instead of 0. Other shells assign 0 as expected.
+
+// expect_failure_for: ksh
+int return0() {
+  int a = 0;
+  return -a;
+}
+
+void putstring(char *s) {
+	while (*s) {
+		putchar(*s);
+		s = s + 1;
+	}
+}
+
+void main() {
+  // Workaround: use `return0() + 0 == 0`.
+  // This forces the result of return0 to be in an arithmetic expansion which removes the `-` from `-0`.
+  if (return0() == 0) {
+    putstring("zero\n");
+  } else {
+    putstring("non-zero\n");
+  }
+}
