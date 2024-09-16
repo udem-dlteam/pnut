c05d7c953602b3e4c9bb3ee2ffae9e588b50f79c
Laurent Huberdeau <laurenthuberd@gmail.com>
2024-09-16 00:11:33 -0400

    Add c4.c/repl.c to compile and exec times bench

--- benchmarks/compile-times/run.sh
+++ benchmarks/compile-times/run.sh
@@ -12,6 +12,12 @@ print_time()
   printf "%s %s\n" "$((ms/1000)).$((ms/100%10))$((ms/10%10))$((ms%10))s" "$2"
 }
 
+comp_program_options() { # $1 = program
+  case $1 in
+    "examples/c4.c") echo "-I./examples/c4-libs/" ;;
+  esac
+}
+
 compile() {
   # shell=$1
   file=$2
@@ -20,13 +26,13 @@ compile() {
   output_name=${4-$name}
 
   if [ $1 = "gcc" ]; then
-    TIME_MS=$(( `bash -c "time $COMP_DIR/pnut-sh-base.exe $file $options > $COMP_DIR/$output_name-with-$1.sh" 2>&1 | fgrep real | sed -e "s/real[^0-9]*//g" -e "s/m/*60000+/g" -e "s/s//g" -e "s/\\+0\\./-1000+1/g" -e "s/\\.//g"` ))
+    TIME_MS=$(( `bash -c "time $COMP_DIR/pnut-sh-base.exe $file $options $(comp_program_options $file) > $COMP_DIR/$output_name-with-$1.sh" 2>&1 | fgrep real | sed -e "s/real[^0-9]*//g" -e "s/m/*60000+/g" -e "s/s//g" -e "s/\\+0\\./-1000+1/g" -e "s/\\.//g"` ))
   elif [ $1 = "pnut" ]; then
-    TIME_MS=$(( `bash -c "time $COMP_DIR/pnut-sh-compiled-by-pnut-exe.exe $file $options > $COMP_DIR/$output_name-with-$1.sh" 2>&1 | fgrep real | sed -e "s/real[^0-9]*//g" -e "s/m/*60000+/g" -e "s/s//g" -e "s/\\+0\\./-1000+1/g" -e "s/\\.//g"` ))
+    TIME_MS=$(( `bash -c "time $COMP_DIR/pnut-sh-compiled-by-pnut-exe.exe $file $options $(comp_program_options $file) > $COMP_DIR/$output_name-with-$1.sh" 2>&1 | fgrep real | sed -e "s/real[^0-9]*//g" -e "s/m/*60000+/g" -e "s/s//g" -e "s/\\+0\\./-1000+1/g" -e "s/\\.//g"` ))
   else
-    TIME_MS=$(( `bash -c "time $1 $COMP_DIR/pnut.sh $file $options > $COMP_DIR/$output_name-with-$1.sh" 2>&1 | fgrep real | sed -e "s/real[^0-9]*//g" -e "s/m/*60000+/g" -e "s/s//g" -e "s/\\+0\\./-1000+1/g" -e "s/\\.//g"` ))
+    TIME_MS=$(( `bash -c "time $1 $COMP_DIR/pnut.sh $file $options $(comp_program_options $file) > $COMP_DIR/$output_name-with-$1.sh" 2>&1 | fgrep real | sed -e "s/real[^0-9]*//g" -e "s/m/*60000+/g" -e "s/s//g" -e "s/\\+0\\./-1000+1/g" -e "s/\\.//g"` ))
   fi
-  print_time $TIME_MS "for: $1 with $file $(sha256sum $COMP_DIR/$output_name-with-$1.sh | cut -d' ' -f1)"
+  print_time $TIME_MS "for: $1 with $file $(sha256sum $COMP_DIR/$output_name-with-$1.sh | cut -d' ' -f1) size: $(wc -l < $file) $(wc -l < $COMP_DIR/$output_name-with-$1.sh)"
 }
 
 PNUT_SH_OPTIONS="-DRT_NO_INIT_GLOBALS -Dsh"
@@ -40,7 +46,7 @@ gcc -o $COMP_DIR/pnut-exe.exe $PNUT_x86_OPTIONS -O3 pnut.c
 
 chmod +x $COMP_DIR/pnut-sh-compiled-by-pnut-exe.exe
 
-programs="examples/empty.c examples/hello.c examples/fib.c examples/cat.c examples/cp.c examples/wc.c examples/sha256sum.c"
+programs="examples/empty.c examples/hello.c examples/fib.c examples/cat.c examples/cp.c examples/wc.c examples/sha256sum.c examples/c4.c examples/repl.c"
 runners="ksh dash bash yash zsh gcc pnut"
 
 for prog in $programs; do
--- benchmarks/example-execution-times/run.sh
+++ benchmarks/example-execution-times/run.sh
@@ -6,7 +6,7 @@ COMP_DIR="$DIR/compiled"
 # Create the compiled directory if it doesn't exist
 mkdir -p $COMP_DIR
 
-programs="examples/sha256sum.c examples/empty.c examples/hello.c examples/fib.c examples/cat.c examples/cp.c examples/wc.c"
+programs="examples/sha256sum.c examples/empty.c examples/hello.c examples/fib.c examples/cat.c examples/cp.c examples/wc.c examples/c4.c examples/repl.c"
 runners="ksh dash bash yash zsh gcc pnut"
 
 print_time()
@@ -29,7 +29,7 @@ run() {
     chmod +x $executable
     TIME_MS=$(( `bash -c "time ./$executable $options > $COMP_DIR/$name-output-with-$1" 2>&1 | fgrep real | sed -e "s/real[^0-9]*//g" -e "s/m/*60000+/g" -e "s/s//g" -e "s/\\+0\\./-1000+1/g" -e "s/\\.//g"` ))
   else
-    TIME_MS=$(( `bash -c "time $1 $COMP_DIR/$name-with-gcc.sh $options > $COMP_DIR/$name-output-with-$1" 2>&1 | fgrep real | sed -e "s/real[^0-9]*//g" -e "s/m/*60000+/g" -e "s/s//g" -e "s/\\+0\\./-1000+1/g" -e "s/\\.//g"` ))
+    TIME_MS=$(( `bash -c "time $1 $COMP_DIR/$name-on-shell.sh $options > $COMP_DIR/$name-output-with-$1" 2>&1 | fgrep real | sed -e "s/real[^0-9]*//g" -e "s/m/*60000+/g" -e "s/s//g" -e "s/\\+0\\./-1000+1/g" -e "s/\\.//g"` ))
   fi
   print_time $TIME_MS "for: $1 with $name on $options $(sha256sum $COMP_DIR/$name-output-with-$1 | cut -d' ' -f1)"
 }
@@ -37,10 +37,21 @@ run() {
 run_program_options() { # $1 = program, $2 = runner
   case $1 in
     "examples/cat.c")       echo "$COMP_DIR/file-64k" ;;
-    "examples/cp.c")        echo "$COMP_DIR/file-64k $COMP_DIR/file-64k-out-$2" ;;
+    "examples/cp.c")
+      touch "$COMP_DIR/file-64k-out-$2" # gcc and pnut only write to the file if it exists
+      echo "$COMP_DIR/file-64k $COMP_DIR/file-64k-out-$2"
+      ;;
     "examples/wc.c")        echo "$COMP_DIR/file-64k" ;;
     "examples/sha256sum.c") echo "$COMP_DIR/file-64k" ;;
+    "examples/c4.c")        echo "examples/c4.c" ;;
+    "examples/repl.c")      echo "< $COMP_DIR/program.scm" ;;
     *)                      echo "" ;;
+  esac
+}
+
+comp_program_options() { # $1 = program
+  case $1 in
+    "examples/c4.c") echo "-I./examples/c4-libs/" ;;
   esac
 }
 
@@ -52,7 +63,13 @@ while [ $i -lt 128 ] ; do
   : $(( i += 1 ))
 done;
 
-PNUT_SH_OPTIONS="-DRT_NO_INIT_GLOBALS -Dsh"
+# and program.scm
+# (define (fib n) (if (< n 2) n (+ (fib (- n 1)) (fib (- n 2))))) (fib 10)
+# (define (factorial n) (if (= n 0) 1 (* n (factorial (- n 1))))) (factorial 10)
+# (display "Hello, world!")
+echo '(display "Hello, world!")' > $COMP_DIR/program.scm
+
+PNUT_SH_OPTIONS="-DRT_NO_INIT_GLOBALS -Dsh -DOPTIMIZE_LONG_LINES"
 PNUT_x86_OPTIONS="-Dtarget_i386_linux"
 #PNUT_x86_OPTIONS="-Dtarget_x86_64_linux"
 #PNUT_x86_OPTIONS="-Dtarget_x86_64_mac"
@@ -67,15 +84,15 @@ for prog in $programs; do
   # Compile using pnut-sh compiled with gcc and pnut.
   # This makes sure that the programs are compiled correctly.
   name=$(basename $prog ".c")
-  $COMP_DIR/pnut-sh-base.exe $prog > $COMP_DIR/$name-with-gcc.sh
-  ksh $COMP_DIR/pnut.sh $prog > $COMP_DIR/$name-on-shell.sh
+  $COMP_DIR/pnut-sh-base.exe $prog $(comp_program_options $prog) > $COMP_DIR/$name-with-gcc.sh
+  ksh $COMP_DIR/pnut.sh $prog $(comp_program_options $prog) > $COMP_DIR/$name-on-shell.sh
 
-  if ! diff "$COMP_DIR/$name-with-gcc.sh" "$COMP_DIR/$name-on-shell.sh" > /dev/null ; then
-    echo "Warning: Compiling with pnut-sh.exe and pnut-sh.sh produced different outputs"
-    continue
-  fi
+  # if ! diff "$COMP_DIR/$name-with-gcc.sh" "$COMP_DIR/$name-on-shell.sh" > /dev/null ; then
+  #   echo "Warning: Compiling $name with pnut-sh.exe and pnut-sh.sh produced different outputs"
+  #   exit
+  # fi
 
   for runner in $runners; do
-    run "$runner" $prog $(run_program_options $prog $runner)
+    run "$runner" $prog "$(run_program_options $prog $runner)"
   done
 done
