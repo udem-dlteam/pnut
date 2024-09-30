0f83a47bb761e3b37704962696ea29d4a6a1ee45
Laurent Huberdeau <laurenthuberd@gmail.com>
2024-09-30 13:13:04 -0400

    Regenerate examples

--- examples/compiled/echo.sh
+++ examples/compiled/echo.sh
@@ -19,8 +19,8 @@ _main() { let argc $2; let argv_ $3
 _put_pstr() {
   : $(($1 = 0)); shift # Return 0
   __addr=$1; shift
-  while [ $((_$__addr)) != 0 ]; do
-    printf \\$((_$__addr/64))$((_$__addr/8%8))$((_$__addr%8))
+  while [ $((__c = _$__addr)) != 0 ]; do
+    printf \\$((__c/64))$((__c/8%8))$((__c%8))
     : $((__addr += 1))
   done
 }
--- examples/compiled/select-file.sh
+++ examples/compiled/select-file.sh
@@ -246,8 +246,8 @@ _getchar() {
 _put_pstr() {
   : $(($1 = 0)); shift # Return 0
   __addr=$1; shift
-  while [ $((_$__addr)) != 0 ]; do
-    printf \\$((_$__addr/64))$((_$__addr/8%8))$((_$__addr%8))
+  while [ $((__c = _$__addr)) != 0 ]; do
+    printf \\$((__c/64))$((__c/8%8))$((__c%8))
     : $((__addr += 1))
   done
 }
