032a2966b11c27ea35ca71058455bce903d451fd
Laurent Huberdeau <laurenthuberd@gmail.com>
2024-12-28 16:20:54 -0500

    Add test for concatenation of strings with NUL

--- tests/_all/strings-with-nul.c
+++ tests/_all/strings-with-nul.c
@@ -4,13 +4,31 @@
 
 char* str1 = "WOWOWOWOWOWO\0HAHAHAHA";
 // Make sure escape sequences spanning 16 characters boundary are handled correctly.
-// This is important
 char *str2 = "012345678901235\0 6789";
 
-int main() {
+// 32 NUL characters, followed by 4 non-NUL characters
+char *str3 = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0test";
+
+// String concatenation and stringizing works with NUL characters
+char *str4 = "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "\0" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z";
+
+void putstrl(char* str, int len) {
   int i = 0;
-  while (i < 20) {
-    putchar(str1[i]);
+  while (i < len) {
+    if (str[i] == '\0') {
+      putchar('_');
+    } else {
+      putchar(str[i]);
+    }
     i += 1;
   }
+  putchar('\n');
+}
+
+int main() {
+  putstrl(str1, 20);
+  putstrl(str2, 20);
+  putstrl(str3 + 32, 4);
+  putstrl(str4, 27);
+  return 0;
 }
