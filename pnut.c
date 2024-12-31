7e98a071c0f90c461e2f21f8215cad246988938c
Robert Burnett <dr.slate2@gmail.com>
2024-12-31 14:44:22 -0600

    move ANSI_* macros inside #ifdef

--- pnut.c
+++ pnut.c
@@ -2016,16 +2016,19 @@ void get_tok() {
 #endif
 
 
-#define ANSI_RED     "\x1b[31m"
-#define ANSI_GREEN   "\x1b[32m"
-#define ANSI_YELLOW  "\x1b[33m"
-#define ANSI_BLUE    "\x1b[34m"
-#define ANSI_MAGENTA "\x1b[35m"
-#define ANSI_CYAN    "\x1b[36m"
-#define ANSI_RESET   "\x1b[0m"
 
 void parse_error(char * msg, int token) {
+
+
 #ifdef NICE_ERR_MSG 
+  #define ANSI_RED     "\x1b[31m"
+  #define ANSI_GREEN   "\x1b[32m"
+  #define ANSI_YELLOW  "\x1b[33m"
+  #define ANSI_BLUE    "\x1b[34m"
+  #define ANSI_MAGENTA "\x1b[35m"
+  #define ANSI_CYAN    "\x1b[36m"
+  #define ANSI_RESET   "\x1b[0m"
+
   //Error header
   putstr(ANSI_RED"Error occurred while parsing ");    
   putstr(ANSI_GREEN"\"");
