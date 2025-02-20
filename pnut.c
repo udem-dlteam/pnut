ca39964c02715fe5ce2f71b65f0bd070fe5589ad
Laurent Huberdeau <laurenthuberd@gmail.com>
2025-02-20 12:36:01 -0500

    Parse float literals and types (Stubbed to integer)

--- pnut.c
+++ pnut.c
@@ -2236,6 +2236,11 @@ void get_tok() {
             tok = tok == INTEGER_LL ? INTEGER_ULL : INTEGER_UL;
             get_ch();
           }
+        } else if (ch == 'f' || ch == '.') {
+          get_ch();
+          tok = INTEGER;
+          while (accum_digit(10)); // Skip the fractional part
+          val = 0; // Force the value to be 0 for now. TODO: Convert to float
         }
 #endif
 
@@ -2809,7 +2814,7 @@ ast parse_type_specifier() {
     case CHAR_KW:
     case INT_KW:
     case VOID_KW:
-#ifdef DEBUG_PARSER
+#ifndef sh
     case FLOAT_KW:
     case DOUBLE_KW:
 #endif
@@ -2842,7 +2847,7 @@ ast parse_type_specifier() {
 
     case LONG_KW:
       get_tok();
-#ifdef DEBUG_PARSER
+#ifndef sh
       if (tok == DOUBLE_KW) {
         get_tok();
         return new_ast0(DOUBLE_KW, 0);
