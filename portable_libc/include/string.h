7ff9b31eb8a77957108f8090c81b8916854b3a1f
Laurent Huberdeau <laurenthuberd@gmail.com>
2025-02-21 15:57:37 -0500

    Improve portable_libc (WIP)

--- portable_libc/include/string.h
+++ portable_libc/include/string.h
@@ -9,6 +9,7 @@ int memcmp(const void *vl, const void *vr, size_t n);
 
 size_t strlen(const char *s);
 char *strcpy(char *dest, const char *src);
+char *strncpy(char *dest, const char *src, size_t n);
 char *strcat(char *dest, const char *src);
 char *strchr(const char *s, int c);
 char *strrchr(const char *s, int c);
@@ -18,5 +19,7 @@ char *strerror(int errnum);
 int strncmp(const char *s1, const char *s2, size_t n);
 char *strpbrk(const char *dest, const char *breakset);
 char *strstr (char *__haystack, char *__needle);
+
+#include "../src/string.c"
 
 #endif
