7f2e8fd4b0c0891601dda827c1432b82de1384fa
Laurent Huberdeau <laurenthuberd@gmail.com>
2025-02-21 15:57:37 -0500

    Improve portable_libc (WIP)

--- portable_libc/include/unistd.h
+++ portable_libc/include/unistd.h
@@ -13,10 +13,19 @@ ssize_t write(int fd, void *buf, size_t count);
 int open(const char *pathname, int flags, mode_t mode);
 int close(int fd);
 
+#define SEEK_CUR 1
+#define SEEK_END 2
+#define SEEK_SET 0
+
 off_t lseek(int fd, off_t offset, int whence);
 int unlink(const char *pathname);
 
 char *getcwd(char *buf, size_t size);
 int execvp(const char *__file, char **__argv);
+
+int unlink(const char *pathname);
+int execvp(const char *__file, char **__argv);
+
+#include "../src/unistd.c"
 
 #endif
