a8e34fadb729bde08debc62f93c53d30d9fcdb78
Laurent Huberdeau <laurenthuberd@gmail.com>
2025-03-11 13:19:17 -0400

    Properly align structs/unions with widest member

--- tests/_exe/sizeof.c
+++ tests/_exe/sizeof.c
@@ -1,42 +1,141 @@
 #include <stdio.h>
 
-// The testing infrastructure doesn't distinguish between 32 bit and 64 bit
-// tests, so we need to divide the size by 2 in the 64 bit case.
-
-#if defined (PNUT_I386)
-#define MUL 1
-#elif defined (PNUT_X86_64)
-#define MUL 2
-#else
-#define MUL 2
-#endif
-
-void putint_aux(int n) {
-  if (n <= -10) putint_aux(n / 10);
-  putchar('0' - (n % 10));
-}
-
 void putint(int n) {
   if (n < 0) {
     putchar('-');
-    putint_aux(n);
-  } else {
-    putint_aux(-n);
+    n = -n;
   }
+  if (n > 9) {
+    putint(n / 10);
+  }
+  putchar('0' + n % 10);
 }
+
+struct S0 {
+  char a;
+}; // 1 byte
+
+struct S1 {
+  int a;      // 4 bytes
+  int b;      // 4 bytes
+}; // 8 bytes
+
+struct S2 {
+  int a;
+  char b;
+  // 3 bytes padding
+  int c;
+}; // 12 bytes
+
+struct S3 {
+  char a;
+  char b[2];
+  // 1 bytes padding
+  int c;
+}; // 8 bytes
+
+struct S4 {
+  char a;       // 1 byte
+  int b[0];     // 4 * 0 = 0 bytes
+}; // 1 byte, largest member is int (1)
+
+struct S5 {
+  char b[4];
+}; // 4 bytes, largest member is char (1)
+
+struct S6 {
+  int a;
+}; // 4 bytes, largest member is int (4)
+
+union U0 {
+  char a;       // 1 byte
+  int b;        // 4 bytes
+}; // 4 bytes
+
+union U1 {
+  char a;       // 1 byte
+  int b;        // 4 bytes
+  char c[5];    // 5 bytes
+}; // 8 bytes because aligned with width of int (4)
+
+union U2 {
+  char a[3];    // 3 bytes
+  int b;        // 4 bytes
+}; // 4 bytes because aligned with width of int (4)
+
+union U3 {
+  char a[5];    // 5 bytes
+  char b;       // 1 byte
+}; // 5 bytes because aligned with width of char (1)
+
+union U4 {
+  char a[5];    // 5 bytes
+  int b;        // 4 bytes
+}; // 8 bytes, because aligned with width of int (4)
+
+union U5 {
+  char a[5];    // 5 bytes
+  struct S5 b;  // 4 bytes
+}; // 5 bytes because aligned with width of char (1)
+
+union U6 {
+  char a[5];
+  struct S6 b;
+}; // 5 bytes
+
+union U7 {
+  char a[9];
+  struct S5 b[1]; // 4 bytes
+}; // 9 bytes because aligned with width of char (1)
+
+union U8 {
+  char a[2][3];   // 6 bytes
+  struct S5 b[1]; // 4 bytes
+}; // 6 bytes because aligned with width of char (1)
+
+union U9 {
+  char a[2][3];   // 6 bytes
+  struct S4 b[1]; // 1 byte, but largest member is int (array of size 0)
+}; // 6 bytes because aligned with width of int (4)
+
+union U10 {
+  char a[2][3];   // 6 bytes
+  struct S5 b[1]; // 4 bytes
+  int  c[1][1][1][1]; // 4 bytes
+}; // 8 bytes because aligned with width of int (4)
 
 int main() {
   int a[10] = { 0, 1, 2, 3 };
   int b[]   = { 0, 1, 2, 3 }; // Infer size from initializer (4 elements)
-  putint(sizeof(char));             putchar('\n'); // No division needed because sizeof(char) is always 1
-  putint(sizeof(int) / MUL);        putchar('\n');
-  putint(sizeof(int[10]) / MUL);    putchar('\n');
-  putint(sizeof(int[10][2]) / MUL); putchar('\n');
-  putint(sizeof(a) / MUL);          putchar('\n'); // sizeof (expr)
-  putint(sizeof a / MUL);           putchar('\n'); // sizeof expr
-  putint(sizeof(b) / MUL);          putchar('\n'); // sizeof (expr)
-  putint(sizeof b / MUL);           putchar('\n'); // sizeof expr
-  putint(sizeof((void *) a) / MUL); putchar('\n'); // sizeof (cast_expr)
+  putint(sizeof(char));       putchar('\n'); // No division needed because sizeof(char) is always 1
+  putint(sizeof(int));        putchar('\n');
+  putint(sizeof(int[10]));    putchar('\n');
+  putint(sizeof(int[10][2])); putchar('\n');
+  putint(sizeof(a));          putchar('\n'); // sizeof (expr)
+  putint(sizeof a);           putchar('\n'); // sizeof expr
+  putint(sizeof(b));          putchar('\n'); // sizeof (expr)
+  putint(sizeof b);           putchar('\n'); // sizeof expr
+  putint(sizeof((void *) a)); putchar('\n'); // sizeof (cast_expr)
+
+  putint(sizeof(struct S0)); putchar('\n');
+  putint(sizeof(struct S1)); putchar('\n');
+  putint(sizeof(struct S2)); putchar('\n');
+  putint(sizeof(struct S3)); putchar('\n');
+  putint(sizeof(struct S4)); putchar('\n');
+
+  putint(sizeof(union U0)); putchar('\n');
+  putint(sizeof(union U1)); putchar('\n');
+  putint(sizeof(union U3)); putchar('\n');
+  putint(sizeof(union U3[1])); putchar('\n');
+  putint(sizeof(union U3[2])); putchar('\n');
+  putint(sizeof(union U3[5])); putchar('\n');
+  putint(sizeof(union U4)); putchar('\n');
+  putint(sizeof(union U5)); putchar('\n');
+  putint(sizeof(union U6)); putchar('\n');
+  putint(sizeof(union U7)); putchar('\n');
+  putint(sizeof(union U8)); putchar('\n');
+  putint(sizeof(union U9)); putchar('\n');
+  putint(sizeof(union U10)); putchar('\n');
 
   return 0;
 }
