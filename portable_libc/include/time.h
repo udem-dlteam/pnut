aff08b337db44388f05db6606826aa3bd1a66797
Laurent Huberdeau <laurenthuberd@gmail.com>
2025-03-25 17:02:08 -0400

    Add struct tm and time/localtime decls to time.h

--- portable_libc/include/time.h
+++ portable_libc/include/time.h
@@ -11,9 +11,24 @@ struct timezone {
   int tz_dsttime;
 };
 
+struct tm {
+  int tm_sec;
+  int tm_min;
+  int tm_hour;
+  int tm_mday;
+  int tm_mon;
+  int tm_year;
+  int tm_wday;
+  int tm_yday;
+  int tm_isdst;
+};
+
 int gettimeofday(struct timeval *tv, struct timezone *tz);
 
 typedef long __time32_t;
 typedef __time32_t time_t;
+
+time_t time (time_t * tloc);
+struct tm *localtime (time_t const *timep);
 
 #endif
