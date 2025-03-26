6d232ef1b3ea6b075e21db571dc032921ae73475
Laurent Huberdeau <laurenthuberd@gmail.com>
2025-03-26 15:19:53 -0400

    Remove rt_free

--- exe.c
+++ exe.c
@@ -2531,10 +2531,6 @@ void rt_malloc() {
   mov_reg_reg(reg_X, reg_Y);              // Return the old bump pointer
 }
 
-void rt_free() {
-  return; // Free are NO-OP
-}
-
 #endif
 
 void codegen_end() {
@@ -2663,7 +2659,7 @@ void codegen_end() {
   // free function
   def_label(free_lbl);
   mov_reg_mem(reg_X, reg_SP, WORD_SIZE);
-  rt_free();
+  // Free is NO-OP
   ret();
 
   // printf function stub
