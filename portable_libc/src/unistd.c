dd1a680ce7f4301f5550432da1c01670811309a7
Laurent Huberdeau <laurenthuberd@gmail.com>
2025-04-03 17:09:34 -0400

    Add access flags to libc

--- portable_libc/src/unistd.c
+++ portable_libc/src/unistd.c
@@ -10,10 +10,18 @@ int open(const char *pathname, int flags, mode_t mode);
 int close(int fd);
 off_t lseek(int fd, off_t offset, int whence);
 int unlink(const char *pathname);
+int mkdir(const char *pathname, mode_t mode);
+int chmod(const char *pathname, mode_t mode);
+int access(const char *pathname, int amode);
 
 */
 
 #ifdef PNUT_CC
+
+#define	R_OK	4		/* Test for read permission.  */
+#define	W_OK	2		/* Test for write permission.  */
+#define	X_OK	1		/* Test for execute permission.  */
+#define	F_OK	0		/* Test for existence.  */
 
 char *getcwd(char *buf, size_t size) {
   /*
