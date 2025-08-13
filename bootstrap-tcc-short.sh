8d9e61e7fa1578327fd39a6f5149493ffab364a6
Laurent Huberdeau <laurenthuberd@gmail.com>
2025-08-13 19:25:20 -0400

    Use separate boot${N}-lib directory for each stage

--- bootstrap-tcc-short.sh
+++ bootstrap-tcc-short.sh
@@ -142,24 +142,28 @@ touch ${TCC_DIR}/config.h
 
 cp mes-config.h $MES_DIR/include/mes/config.h
 
-mkdir -p "$TEMP_DIR/boot0"
-mkdir -p "$TEMP_DIR/boot0-lib"
-mkdir -p "$TEMP_DIR/boot0-lib/tcc"
-
 # Create unified libc file
-catm "$TEMP_DIR/boot0/unified-libc.c" "$MES_DIR/lib" ctype/isalnum.c ctype/isalpha.c ctype/isascii.c ctype/iscntrl.c ctype/isdigit.c ctype/isgraph.c ctype/islower.c ctype/isnumber.c ctype/isprint.c ctype/ispunct.c ctype/isspace.c ctype/isupper.c ctype/isxdigit.c ctype/tolower.c ctype/toupper.c dirent/closedir.c dirent/__getdirentries.c dirent/opendir.c linux/readdir.c linux/access.c linux/brk.c linux/chdir.c linux/chmod.c linux/clock_gettime.c linux/close.c linux/dup2.c linux/dup.c linux/execve.c linux/fcntl.c linux/fork.c linux/fsync.c linux/fstat.c linux/_getcwd.c linux/getdents.c linux/getegid.c linux/geteuid.c linux/getgid.c linux/getpid.c linux/getppid.c linux/getrusage.c linux/gettimeofday.c linux/getuid.c linux/ioctl.c linux/ioctl3.c linux/kill.c linux/link.c linux/lseek.c linux/lstat.c linux/malloc.c linux/mkdir.c linux/mknod.c linux/nanosleep.c linux/_open3.c linux/pipe.c linux/_read.c linux/readlink.c linux/rename.c linux/rmdir.c linux/setgid.c linux/settimer.c linux/setuid.c linux/signal.c linux/sigprogmask.c linux/symlink.c linux/stat.c linux/time.c linux/unlink.c linux/waitpid.c linux/wait4.c linux/${MES_ARCH}-mes-gcc/_exit.c linux/${MES_ARCH}-mes-gcc/syscall.c linux/${MES_ARCH}-mes-gcc/_write.c math/ceil.c math/fabs.c math/floor.c mes/abtod.c mes/abtol.c mes/__assert_fail.c mes/assert_msg.c mes/__buffered_read.c mes/__init_io.c mes/cast.c mes/dtoab.c mes/eputc.c mes/eputs.c mes/fdgetc.c mes/fdgets.c mes/fdputc.c mes/fdputs.c mes/fdungetc.c mes/globals.c mes/itoa.c mes/ltoab.c mes/ltoa.c mes/__mes_debug.c mes/mes_open.c mes/ntoab.c mes/oputc.c mes/oputs.c mes/search-path.c mes/ultoa.c mes/utoa.c posix/alarm.c posix/buffered-read.c posix/execl.c posix/execlp.c posix/execv.c posix/execvp.c posix/getcwd.c posix/getenv.c posix/isatty.c posix/mktemp.c posix/open.c posix/pathconf.c posix/raise.c posix/sbrk.c posix/setenv.c posix/sleep.c posix/unsetenv.c posix/wait.c posix/write.c stdio/clearerr.c stdio/fclose.c stdio/fdopen.c stdio/feof.c stdio/ferror.c stdio/fflush.c stdio/fgetc.c stdio/fgets.c stdio/fileno.c stdio/fopen.c stdio/fprintf.c stdio/fputc.c stdio/fputs.c stdio/fread.c stdio/freopen.c stdio/fscanf.c stdio/fseek.c stdio/ftell.c stdio/fwrite.c stdio/getc.c stdio/getchar.c stdio/perror.c stdio/printf.c stdio/putc.c stdio/putchar.c stdio/remove.c stdio/snprintf.c stdio/sprintf.c stdio/sscanf.c stdio/ungetc.c stdio/vfprintf.c stdio/vfscanf.c stdio/vprintf.c stdio/vsnprintf.c stdio/vsprintf.c stdio/vsscanf.c stdlib/abort.c stdlib/abs.c stdlib/alloca.c stdlib/atexit.c stdlib/atof.c stdlib/atoi.c stdlib/atol.c stdlib/calloc.c stdlib/__exit.c stdlib/exit.c stdlib/free.c stdlib/mbstowcs.c stdlib/puts.c stdlib/qsort.c stdlib/realloc.c stdlib/strtod.c stdlib/strtof.c stdlib/strtol.c stdlib/strtold.c stdlib/strtoll.c stdlib/strtoul.c stdlib/strtoull.c string/bcmp.c string/bcopy.c string/bzero.c string/index.c string/memchr.c string/memcmp.c string/memcpy.c string/memmem.c string/memmove.c string/memset.c string/rindex.c string/strcat.c string/strchr.c string/strcmp.c string/strcpy.c string/strcspn.c string/strdup.c string/strerror.c string/strlen.c string/strlwr.c string/strncat.c string/strncmp.c string/strncpy.c string/strpbrk.c string/strrchr.c string/strspn.c string/strstr.c string/strupr.c stub/atan2.c stub/bsearch.c stub/chown.c stub/__cleanup.c stub/cos.c stub/ctime.c stub/exp.c stub/fpurge.c stub/freadahead.c stub/frexp.c stub/getgrgid.c stub/getgrnam.c stub/getlogin.c stub/getpgid.c stub/getpgrp.c stub/getpwnam.c stub/getpwuid.c stub/gmtime.c stub/ldexp.c stub/localtime.c stub/log.c stub/mktime.c stub/modf.c stub/mprotect.c stub/pclose.c stub/popen.c stub/pow.c stub/putenv.c stub/rand.c stub/realpath.c stub/rewind.c stub/setbuf.c stub/setgrent.c stub/setlocale.c stub/setvbuf.c stub/sigaction.c stub/sigaddset.c stub/sigblock.c stub/sigdelset.c stub/sigemptyset.c stub/sigsetmask.c stub/sin.c stub/sys_siglist.c stub/system.c stub/sqrt.c stub/strftime.c stub/times.c stub/ttyname.c stub/umask.c stub/utime.c ${MES_ARCH}-mes-gcc/setjmp.c
+catm "$TEMP_DIR/unified-libc.c" "$MES_DIR/lib" ctype/isalnum.c ctype/isalpha.c ctype/isascii.c ctype/iscntrl.c ctype/isdigit.c ctype/isgraph.c ctype/islower.c ctype/isnumber.c ctype/isprint.c ctype/ispunct.c ctype/isspace.c ctype/isupper.c ctype/isxdigit.c ctype/tolower.c ctype/toupper.c dirent/closedir.c dirent/__getdirentries.c dirent/opendir.c linux/readdir.c linux/access.c linux/brk.c linux/chdir.c linux/chmod.c linux/clock_gettime.c linux/close.c linux/dup2.c linux/dup.c linux/execve.c linux/fcntl.c linux/fork.c linux/fsync.c linux/fstat.c linux/_getcwd.c linux/getdents.c linux/getegid.c linux/geteuid.c linux/getgid.c linux/getpid.c linux/getppid.c linux/getrusage.c linux/gettimeofday.c linux/getuid.c linux/ioctl.c linux/ioctl3.c linux/kill.c linux/link.c linux/lseek.c linux/lstat.c linux/malloc.c linux/mkdir.c linux/mknod.c linux/nanosleep.c linux/_open3.c linux/pipe.c linux/_read.c linux/readlink.c linux/rename.c linux/rmdir.c linux/setgid.c linux/settimer.c linux/setuid.c linux/signal.c linux/sigprogmask.c linux/symlink.c linux/stat.c linux/time.c linux/unlink.c linux/waitpid.c linux/wait4.c linux/${MES_ARCH}-mes-gcc/_exit.c linux/${MES_ARCH}-mes-gcc/syscall.c linux/${MES_ARCH}-mes-gcc/_write.c math/ceil.c math/fabs.c math/floor.c mes/abtod.c mes/abtol.c mes/__assert_fail.c mes/assert_msg.c mes/__buffered_read.c mes/__init_io.c mes/cast.c mes/dtoab.c mes/eputc.c mes/eputs.c mes/fdgetc.c mes/fdgets.c mes/fdputc.c mes/fdputs.c mes/fdungetc.c mes/globals.c mes/itoa.c mes/ltoab.c mes/ltoa.c mes/__mes_debug.c mes/mes_open.c mes/ntoab.c mes/oputc.c mes/oputs.c mes/search-path.c mes/ultoa.c mes/utoa.c posix/alarm.c posix/buffered-read.c posix/execl.c posix/execlp.c posix/execv.c posix/execvp.c posix/getcwd.c posix/getenv.c posix/isatty.c posix/mktemp.c posix/open.c posix/pathconf.c posix/raise.c posix/sbrk.c posix/setenv.c posix/sleep.c posix/unsetenv.c posix/wait.c posix/write.c stdio/clearerr.c stdio/fclose.c stdio/fdopen.c stdio/feof.c stdio/ferror.c stdio/fflush.c stdio/fgetc.c stdio/fgets.c stdio/fileno.c stdio/fopen.c stdio/fprintf.c stdio/fputc.c stdio/fputs.c stdio/fread.c stdio/freopen.c stdio/fscanf.c stdio/fseek.c stdio/ftell.c stdio/fwrite.c stdio/getc.c stdio/getchar.c stdio/perror.c stdio/printf.c stdio/putc.c stdio/putchar.c stdio/remove.c stdio/snprintf.c stdio/sprintf.c stdio/sscanf.c stdio/ungetc.c stdio/vfprintf.c stdio/vfscanf.c stdio/vprintf.c stdio/vsnprintf.c stdio/vsprintf.c stdio/vsscanf.c stdlib/abort.c stdlib/abs.c stdlib/alloca.c stdlib/atexit.c stdlib/atof.c stdlib/atoi.c stdlib/atol.c stdlib/calloc.c stdlib/__exit.c stdlib/exit.c stdlib/free.c stdlib/mbstowcs.c stdlib/puts.c stdlib/qsort.c stdlib/realloc.c stdlib/strtod.c stdlib/strtof.c stdlib/strtol.c stdlib/strtold.c stdlib/strtoll.c stdlib/strtoul.c stdlib/strtoull.c string/bcmp.c string/bcopy.c string/bzero.c string/index.c string/memchr.c string/memcmp.c string/memcpy.c string/memmem.c string/memmove.c string/memset.c string/rindex.c string/strcat.c string/strchr.c string/strcmp.c string/strcpy.c string/strcspn.c string/strdup.c string/strerror.c string/strlen.c string/strlwr.c string/strncat.c string/strncmp.c string/strncpy.c string/strpbrk.c string/strrchr.c string/strspn.c string/strstr.c string/strupr.c stub/atan2.c stub/bsearch.c stub/chown.c stub/__cleanup.c stub/cos.c stub/ctime.c stub/exp.c stub/fpurge.c stub/freadahead.c stub/frexp.c stub/getgrgid.c stub/getgrnam.c stub/getlogin.c stub/getpgid.c stub/getpgrp.c stub/getpwnam.c stub/getpwuid.c stub/gmtime.c stub/ldexp.c stub/localtime.c stub/log.c stub/mktime.c stub/modf.c stub/mprotect.c stub/pclose.c stub/popen.c stub/pow.c stub/putenv.c stub/rand.c stub/realpath.c stub/rewind.c stub/setbuf.c stub/setgrent.c stub/setlocale.c stub/setvbuf.c stub/sigaction.c stub/sigaddset.c stub/sigblock.c stub/sigdelset.c stub/sigemptyset.c stub/sigsetmask.c stub/sin.c stub/sys_siglist.c stub/system.c stub/sqrt.c stub/strftime.c stub/times.c stub/ttyname.c stub/umask.c stub/utime.c ${MES_ARCH}-mes-gcc/setjmp.c
 
-# Compile the mes C library for tcc-boot0
-
-go() { # $1: name of bootstrap comp, $2: name of new compiler, $3: additional options for TCC
+go() { # $1: name of bootstrap comp, $2: name of new compiler, $3: lib path (= $2 if empty)
   CC="$1"
+  NEW_CC="$2" # Suffix of new compiler: "boot0", "boot1", "boot2", ...
+  if [ $# -lt 3 ]; then
+    LIB_PATH="$TEMP_DIR/$NEW_CC-lib"
+  else
+    LIB_PATH="$TEMP_DIR/$3-lib"
+  fi
+
+  mkdir -p "$LIB_PATH"
+  mkdir -p "$LIB_PATH/tcc"
+
   for file in "crt1" "crtn" "crti"; do
     $CC -c                                                                     \
       -D HAVE_CONFIG_H=1                                                       \
       -I $MES_DIR/include                                                      \
       -I $MES_DIR/include/linux/${MES_ARCH}                                    \
       $MES_DIR/lib/linux/${MES_ARCH}-mes-gcc/$file.c                           \
-      -o $TEMP_DIR/boot0-lib/$file.o
+      -o $LIB_PATH/$file.o
   done
 
   # libc+gcc.a
@@ -167,20 +171,20 @@ go() { # $1: name of bootstrap comp, $2: name of new compiler, $3: additional op
     -D HAVE_CONFIG_H=1                                                         \
     -I $MES_DIR/include                                                        \
     -I $MES_DIR/include/linux/${MES_ARCH}                                      \
-    $TEMP_DIR/boot0/unified-libc.c                                             \
-    -o $TEMP_DIR/boot0-lib/unified-libc.o
-  $CC -ar cr $TEMP_DIR/boot0-lib/libc.a $TEMP_DIR/boot0-lib/unified-libc.o
+    $TEMP_DIR/unified-libc.c                                                   \
+    -o $LIB_PATH/unified-libc.o
+  $CC -ar cr $LIB_PATH/libc.a $LIB_PATH/unified-libc.o
 
   # libtcc1.a
-  $CC -c -o $TEMP_DIR/boot0-lib/libtcc1.o kit/libtcc1.c
-  $CC -ar cr $TEMP_DIR/boot0-lib/tcc/libtcc1.a $TEMP_DIR/boot0-lib/libtcc1.o
+  $CC -c -o $LIB_PATH/libtcc1.o kit/libtcc1.c
+  $CC -ar cr $LIB_PATH/tcc/libtcc1.a $LIB_PATH/libtcc1.o
 
-  # We can now compile tcc-boot0
+  # We can now compile tcc-$NEW_CC
   $CC \
       -g \
       -v \
       -static \
-      -o $TEMP_DIR/tcc-$2 \
+      -o $TEMP_DIR/tcc-$NEW_CC \
       -D BOOTSTRAP=1 \
       -D HAVE_FLOAT=1 \
       -D HAVE_BITFIELD=1 \
@@ -188,11 +192,11 @@ go() { # $1: name of bootstrap comp, $2: name of new compiler, $3: additional op
       -D HAVE_SETJMP=1 \
       -I $MES_DIR/include \
       -D TCC_TARGET_${TCC_TARGET_ARCH}=1 \
-      -D CONFIG_TCCDIR=\"$TEMP_DIR/boot0-lib/tcc\" \
-      -D CONFIG_TCC_CRTPREFIX=\"$TEMP_DIR/boot0-lib\" \
-      -D CONFIG_TCC_LIBPATHS=\"$TEMP_DIR/boot0-lib:$TEMP_DIR/boot0-lib/tcc\" \
+      -D CONFIG_TCCDIR=\"$LIB_PATH/tcc\" \
+      -D CONFIG_TCC_CRTPREFIX=\"$LIB_PATH\" \
+      -D CONFIG_TCC_LIBPATHS=\"$LIB_PATH:$LIB_PATH/tcc\" \
       -D CONFIG_TCC_SYSINCLUDEPATHS=\"$MES_DIR/include\" \
-      -D TCC_LIBGCC=\"$TEMP_DIR/boot0-lib/libc.a\" \
+      -D TCC_LIBGCC=\"$LIB_PATH/libc.a\" \
       -D TCC_LIBTCC1=\"libtcc1.a\" \
       -D CONFIG_TCC_ELFINTERP=\"/mes/loader\" \
       -D CONFIG_TCCBOOT=1 \
@@ -200,19 +204,21 @@ go() { # $1: name of bootstrap comp, $2: name of new compiler, $3: additional op
       -D CONFIG_USE_LIBGCC=1 \
       -D TCC_VERSION=\"0.9.26\" \
       -D ONE_SOURCE=1 \
-      -L $TEMP_DIR/boot0-lib \
+      -L $LIB_PATH \
       $TCC_DIR/tcc.c
 
-  sha256sum $TEMP_DIR/boot0-lib/crt1.o $TEMP_DIR/boot0-lib/crtn.o $TEMP_DIR/boot0-lib/crti.o
+  sha256sum $LIB_PATH/crt1.o $LIB_PATH/crtn.o $LIB_PATH/crti.o
 }
 
 go "$TEMP_DIR/tcc-pnut" "boot0"
 go "$TEMP_DIR/tcc-boot0" "boot1"
 go "$TEMP_DIR/tcc-boot1" "boot2"
 # Make sure we've reached a fixed point
-go "$TEMP_DIR/tcc-boot2" "boot3"
+go "$TEMP_DIR/tcc-boot2" "boot3" "boot2"
 
 # Confirm with hashes that we're at a fixed point
-sha256sum $TEMP_DIR/boot0-lib/crt1.o $TEMP_DIR/boot0-lib/crtn.o $TEMP_DIR/boot0-lib/crti.o
-sha256sum $TEMP_DIR/boot0-lib/libtcc1.o $TEMP_DIR/boot0-lib/tcc/libtcc1.a $TEMP_DIR/boot0-lib/unified-libc.o $TEMP_DIR/boot0-lib/libc.a
+sha256sum $TEMP_DIR/boot0-lib/crt1.o $TEMP_DIR/boot1-lib/crt1.o $TEMP_DIR/boot2-lib/crt1.o
+sha256sum $TEMP_DIR/boot0-lib/tcc/libtcc1.a $TEMP_DIR/boot0-lib/libc.a
+sha256sum $TEMP_DIR/boot1-lib/tcc/libtcc1.a $TEMP_DIR/boot1-lib/libc.a
+sha256sum $TEMP_DIR/boot2-lib/tcc/libtcc1.a $TEMP_DIR/boot2-lib/libc.a
 sha256sum $TEMP_DIR/tcc-boot0 $TEMP_DIR/tcc-boot1 $TEMP_DIR/tcc-boot2 $TEMP_DIR/tcc-boot3
