5178c375fd3890a1cc4c9750a4172ac8963e0cd4
Laurent Huberdeau <laurenthuberd@gmail.com>
2025-08-19 23:53:23 -0400

    Add chmod to bintools

--- kit/bintools.c
+++ kit/bintools.c
@@ -76,6 +76,80 @@ int ENTRY_POINT(int argc, char **argv) {
   return 0;
 }
 #undef ENTRY_POINT
+#define ENTRY_POINT chmod_main
+/*
+ * chmod.c: Change the file mode (permissions) of the specified files
+ *
+ * Usage: ./chmod.sh [--verbose] MODE FILE...
+ */
+
+
+#ifdef FLAT_INCLUDES
+#else
+#endif
+
+#ifndef ENTRY_POINT
+#define ENTRY_POINT main
+#endif
+
+int ENTRY_POINT(int argc, char **argv) {
+  int verbose = 0;
+  int permissions = -1;
+
+  while (argc > 1) {
+    if (strcmp(argv[1], "--verbose") == 0) {
+      verbose = 1;
+      argv += 1;
+      argc -= 1;
+    } else if (strcmp(argv[1], "--help") == 0) {
+      printf("Usage: chmod [--verbose] MODE FILE...\n");
+      exit(0);
+    } else if ('0' <= argv[1][0] && argv[1][0] <= '7') {
+      if (permissions != -1) { break; } // only one mode allowed, this must be a file starting with a digit
+      permissions = 0;
+      int i;
+      for (i = 0; argv[1][i] != '\0' && i < 4; i++) {
+        permissions = (permissions << 3) | (argv[1][i] - '0');
+      }
+      argv += 1;
+      argc -= 1;
+    } else {
+      break;
+    }
+  }
+
+  if (permissions == -1) {
+    fprintf(stderr, "chmod: missing mode\n");
+    exit(1);
+  }
+
+  if (argc < 2) {
+    fprintf(stderr, "chmod: missing file\n");
+    exit(1);
+  }
+
+  while (argc > 1) {
+    if (access(argv[1], F_OK) == -1) {
+      fprintf(stderr, "chmod: cannot access '%s'\n", argv[1]);
+      exit(1);
+    }
+
+    if (verbose) {
+      printf("chmod: changing mode of '%s' to %o\n", argv[1], permissions);
+    }
+
+    if (chmod(argv[1], permissions) == -1) {
+      fprintf(stderr, "chmod: cannot change mode of '%s'\n", argv[1]);
+      exit(1);
+    }
+
+    argv += 1;
+    argc -= 1;
+  }
+
+  return 0;
+}
+#undef ENTRY_POINT
 #define ENTRY_POINT cp_main
 /*
  * cp.c: Copy the contents of one file to another
@@ -1920,10 +1994,12 @@ int invoke_subcommand(char* name, int argc, char **argv) {
       // Shift arguments to pass to subcommand
       return invoke_subcommand(argv[1], argc - 1, argv + 1);
     }
-  } else if (strcmp(name, "cp") == 0) {
-    return cp_main(argc, argv);
   } else if (strcmp(name, "cat") == 0) {
     return cat_main(argc, argv);
+  } else if (strcmp(name, "chmod") == 0) {
+    return chmod_main(argc, argv);
+  } else if (strcmp(name, "cp") == 0) {
+    return cp_main(argc, argv);
   } else if (strcmp(name, "mkdir") == 0) {
     return mkdir_main(argc, argv);
   } else if (strcmp(name, "sha256sum") == 0) {
