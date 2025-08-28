cbc6589cd42be12ac7d98ec5f4d699b88a7c8436
Laurent Huberdeau <laurenthuberd@gmail.com>
2025-08-28 11:52:51 -0400

    Cleaner prepare.sh

--- examples/prepare.sh
+++ examples/prepare.sh
@@ -6,16 +6,17 @@ fail() { echo "$1"; exit $2; }
 
 DIR="examples/"
 COMP_DIR="$DIR/compiled"
+TEMP_DIR="build"
 
-mkdir -p $COMP_DIR
-mkdir -p build
+mkdir -p "$COMP_DIR"
+mkdir -p "$TEMP_DIR"
 
 echo "Compiling examples"
 
 PNUT_SH_OPTIONS="-DRELEASE_PNUT_SH -DRT_COMPACT -DSAFE_MODE"
 
 # Compile pnut.exe
-gcc -o build/pnut-sh-base.exe $PNUT_SH_OPTIONS pnut.c 2> /dev/null || fail "Error: Failed to compile pnut"
+gcc -o $TEMP_DIR/pnut-sh-base.exe $PNUT_SH_OPTIONS pnut.c 2> /dev/null || fail "Error: Failed to compile pnut"
 
 pnut_compile_options() {
   echo `sed -n -e "/\/\/ pnut-options:/p" "$1" | sed -e "s/^\/\/ pnut-options://" |  tr '\n' ',' | sed -e 's/,$//'`
@@ -27,14 +28,14 @@ compile_options() {
 
 failed=0
 
-generate_executable_with() { # $1 = executable, $2 = options
-  if [ $# = 1 ]; then
+generate_executable_with() { # $1 = executable, $2 = file, $3 = options
+  if [ $# = 2 ]; then
     opt=""
   else
-    opt=$2
+    opt=$3
   fi
 
-  if timeout 3 ./build/$1 $file $opt > $COMP_DIR/$filename.sh; then
+  if timeout 3 ./$1 $2 $opt > $COMP_DIR/$filename.sh; then
     chmod +x $COMP_DIR/$filename.sh
     printf "✅\n"
   elif [ $? -eq 124 ]; then
@@ -52,13 +53,13 @@ for file in $(find examples -type f -name "*.c" | sort); do
 
   # To speed up the compilation process, we only compile pnut.exe if there are specific options
   if [ -z "$pnut_opts" ]; then
-    printf "Compiling1 $filename ${file_opts:+"$file_opts "}"
-    generate_executable_with "pnut-sh-base.exe" "$file_opts"
+    printf "Compiling $filename ${file_opts:+"$file_opts "}"
+    generate_executable_with "$TEMP_DIR/pnut-sh-base.exe" "$file" "$file_opts"
   else
-    printf "Compiling2 $filename with $pnut_opts ${file_opts:+"$file_opts "}"
+    printf "Compiling $filename with $pnut_opts ${file_opts:+"$file_opts "}"
     # Compile pnut.exe with specific options
-    gcc -o build/pnut-sh-opt.exe $PNUT_SH_OPTIONS $pnut_opts pnut.c 2> /dev/null || fail "Error: Failed to compile pnut with $pnut_opts"
-    generate_executable_with "pnut-sh-opt.exe" "$file_opts"
+    gcc -o "$TEMP_DIR/pnut-sh-opt.exe" $PNUT_SH_OPTIONS $pnut_opts pnut.c 2> /dev/null || fail "Error: Failed to compile pnut with $pnut_opts"
+    generate_executable_with "$TEMP_DIR/pnut-sh-opt.exe" "$file" "$file_opts"
   fi
 done
 
