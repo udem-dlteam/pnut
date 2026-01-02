7851a8cbf43cf47cab6365c8b993f0f8b62293bb
Laurent Huberdeau <laurenthuberd@gmail.com>
2026-01-02 18:41:23 -0500

    Add support for tcc-0.9.26 to kit/bootstrap.sh

--- kit/benchmark-bootstrap.sh
+++ kit/benchmark-bootstrap.sh
@@ -14,17 +14,20 @@ printf_timing() {
 # Make sure the jammed.sh script is up-to-date
 ./kit/make-jammed.sh
 
-for shell in ksh dash bash yash osh zsh; do
+for BOOTSTRAP_SHELL in ksh dash bash yash osh zsh; do
   root_dir=$(pwd)
-  rm -rf "$TEMP_DIR/$shell" # Make sure it's empty to start fresh
-  mkdir -p "$TEMP_DIR/$shell"
-  cp kit/jammed.sh "$TEMP_DIR/$shell/jammed.sh"
-  cd "$TEMP_DIR/$shell"
+  rm -rf "$TEMP_DIR/$BOOTSTRAP_SHELL" # Make sure it's empty to start fresh
+  mkdir -p "$TEMP_DIR/$BOOTSTRAP_SHELL"
+  cp kit/jammed.sh "$TEMP_DIR/$BOOTSTRAP_SHELL/jammed.sh"
 
+  # Skip ahead to TCC bootstrap
+  # gcc pnut.c -o $TEMP_DIR/$BOOTSTRAP_SHELL/pnut-exe -Dtarget_i386_linux -DONE_PASS_GENERATOR -DBOOTSTRAP_TCC
+
+  cd "$TEMP_DIR/$BOOTSTRAP_SHELL"
   chmod +x jammed.sh
   printf_timing \
-    "jammed.sh with $shell" \
-    "$shell jammed.sh"
-  SHELL="$shell" time bash bootstrap.sh
+    "jammed.sh with $BOOTSTRAP_SHELL" \
+    "$BOOTSTRAP_SHELL jammed.sh"
+  BOOTSTRAP_SHELL="$BOOTSTRAP_SHELL" time bash bootstrap.sh
   cd "$root_dir"
 done
