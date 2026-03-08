3211f1d859defd3941fe47281883bce4b778651e
Laurent Huberdeau <laurenthuberd@gmail.com>
2026-03-07 22:29:40 -0500

    Remove printf from debug module

--- debug.c
+++ debug.c
@@ -4,8 +4,14 @@
 
 void signal_callback_handler(int signum) {
   if (signum == SIGINT) {
-    printf("Caught signal %d\n",signum);
-    printf("Tokenizer at %s:%d:%d\n", fp_filepath, line_number, column_number);
+    putstr("Caught SIGINT (Ctrl+C), exiting gracefully...\n");
+    putstr("Tokenizer at ");
+    putstr(fp_filepath);
+    putstr(":");
+    putint(line_number);
+    putstr(":");
+    putint(column_number);
+    putstr("\n");
     exit(1);
   }
 }
@@ -335,10 +341,12 @@ void print_tok_type(int tok) {
     putchar(tok);
   }
   else {
-    printf("tok=%d\n", tok);
+    putstr("tok="); putint(tok); putstr("\n");
     fatal_error("print_tok_type: unknown token");
   }
 }
+
+#ifdef DEBUG_PARSER
 
 void ast_to_sexp(ast obj);
 
@@ -353,26 +361,26 @@ void ast_list_to_sexp(ast obj) {
 void type_ast_to_sexp(ast type) {
   switch (get_op(type)) {
     case '*':
-      printf("(* ");
+      putstr("(* ");
       type_ast_to_sexp(get_child_('*', type, 1));
-      printf(")");
+      putstr(")");
       break;
 
     case '[':
-      printf("[");
+      putstr("[");
       type_ast_to_sexp(get_child_('[', type, 0));
-      printf(" ");
+      putstr(" ");
       putint(get_child_('[', type, 1));
-      printf("]");
+      putstr("]");
       break;
 
     case '(':
-      printf("(-> (");
+      putstr("(-> (");
       ast_list_to_sexp(get_child_opt_('(', LIST, type, 1)); // Function args
-      if (get_child_('(', type, 2)) printf(" ..."); // Varargs
-      printf(") ");
+      if (get_child_('(', type, 2)) putstr(" ..."); // Varargs
+      putstr(") ");
       type_ast_to_sexp(get_child_('(', type, 0));
-      printf(")");
+      putstr(")");
       break;
 
     case CHAR_KW:
@@ -392,17 +400,19 @@ void type_ast_to_sexp(ast type) {
     case UNION_KW:
 #endif
     case ENUM_KW:
-      printf("(");
+      putstr("(");
       print_tok_type(get_op(type));
-      printf(" ");
+      putstr(" ");
       ast_to_sexp(get_child(type, 0)); // Struct/union name
-      printf(" ");
+      putstr(" ");
       ast_list_to_sexp(get_child(type, 2)); // Struct/union members
-      printf(")");
+      putstr(")");
       break;
 
     default:
-      printf("<Unknown type %d>", get_op(type));
+      putstr("<Unknown type ");
+      putint(get_op(type));
+      putstr(">");
       exit(1);
   }
 }
@@ -411,7 +421,7 @@ void ast_to_sexp(ast obj) {
   int i = 0;
 
   if (obj == 0) {
-    printf("#f");
+    putstr("#f");
     return;
   }
 
@@ -438,7 +448,7 @@ void ast_to_sexp(ast obj) {
       //   print_string_char(get_val_(CHARACTER, obj));
       //   putchar('\'');
       // }
-      printf("(char ");
+      putstr("(char ");
       putint(get_val_(CHARACTER, obj));
       putchar(')');
       break;
@@ -449,9 +459,9 @@ void ast_to_sexp(ast obj) {
       break;
 
     case TYPEDEF_KW:
-      printf("(typedef ");
+      putstr("(typedef ");
       ast_list_to_sexp(get_child_opt_(TYPEDEF_KW, LIST, obj, 0));
-      printf(")");
+      putstr(")");
       break;
 
 #ifdef SUPPORT_STRUCT_UNION
@@ -472,61 +482,61 @@ void ast_to_sexp(ast obj) {
         putchar(' ');
         ast_to_sexp(get_child_(DECL, obj, 2));
       }
-      printf(")");
+      putstr(")");
       break;
 
     case FUN_DECL:
-      printf("(define-fun ");
+      putstr("(define-fun ");
       putstr(symbol_buf(get_val_(IDENTIFIER, get_child__(DECL, IDENTIFIER, get_child__(FUN_DECL, DECL, obj, 0), 0))));
       putchar(' ');
       type_ast_to_sexp(get_child_(DECL, get_child__(FUN_DECL, DECL, obj, 0), 1)); // Get type out of decl
       putchar(' ');
       ast_to_sexp(get_child_(FUN_DECL, obj, 1)); // Body
-      printf(")");
+      putstr(")");
       break;
 
     case CAST:
-      printf("(cast ");
+      putstr("(cast ");
       type_ast_to_sexp(get_child_(DECL, get_child__(CAST, DECL, obj, 0), 1)); // Get type out of decl
-      printf(" ");
+      putstr(" ");
       ast_to_sexp(get_child_(CAST, obj, 1));
-      printf(")");
+      putstr(")");
       break;
 
 #ifdef SUPPORT_SIZEOF
     case SIZEOF_KW:
-      printf("(sizeof ");
+      putstr("(sizeof ");
       if (get_op(get_child_(SIZEOF_KW, obj, 0)) == DECL) {
         type_ast_to_sexp(get_child_(DECL, get_child_(SIZEOF_KW, obj, 0), 1));
       } else {
         ast_to_sexp(get_child_(SIZEOF_KW, obj, 0));
       }
-      printf(")");
+      putstr(")");
       break;
 #endif
 
     case '[':
-      printf("(array_at ");
+      putstr("(array_at ");
       ast_to_sexp(get_child_('[', obj, 0));
-      printf(" ");
+      putstr(" ");
       ast_to_sexp(get_child_('[', obj, 1));
-      printf(")");
+      putstr(")");
       break;
 
     case '(': // Function calls, we print the function and its arguments
-      printf("(");
+      putstr("(");
       ast_to_sexp(get_child_('(', obj, 0));
       if (get_child_('(', obj, 1) != 0) {
-        printf(" ");
+        putstr(" ");
         ast_to_sexp(get_child_('(', obj, 1));
       }
-      printf(")");
+      putstr(")");
       break;
 
     case LIST:
-      printf("(list ");
+      putstr("(list ");
       ast_list_to_sexp(obj);
-      printf(")");
+      putstr(")");
       break;
 
     case '{':
@@ -550,19 +560,9 @@ void ast_to_sexp(ast obj) {
   }
 }
 
-// void show_struct(ast struct_type) {
-//   ast members = get_child(canonicalize_type(struct_type), 2);
+#endif
 
-//   char* name = string_pool + get_val(get_val(get_child(struct_type, 1)));
-
-//   printf("##### Struct %s #####\n", name);
-//   printf("sizeof(%s) = %d\n", name, struct_size(struct_type));
-
-//   while (get_op(members) == ',') {
-//     printf("%s = obj[%d]\n", string_pool + get_val(get_val(get_child(members, 0))), struct_member_offset(struct_type, get_child(members, 0)));
-//     members = get_child(members, 2);
-//   }
-// }
+#if DEBUG_CPP
 
 void print_tokens(int tokens) {
   while (tokens != 0) {
@@ -581,9 +581,14 @@ void print_macro_args(int args) {
 
 void print_macro_ctx(int ix, int ident, int tokens, int args) {
   if (ident == 0) {
-    printf("# %-3d: <unnamed>", ix);
+    putstr("# ");
+    putint(ix);
+    putstr(": <unnamed>");
   } else {
-    printf("# %-3d: %s", ix, symbol_buf(ident));
+    putstr("# ");
+    putint(ix);
+    putstr(": ");
+    putstr(symbol_buf(ident));
   }
   if (args) {
     putchar('(');
@@ -591,7 +596,7 @@ void print_macro_ctx(int ix, int ident, int tokens, int args) {
     putchar(')');
   }
   if (tokens != 0) {
-    printf(" -> ");
+    putstr(" -> ");
     print_tokens(tokens);
   }
 }
@@ -607,3 +612,5 @@ void print_macro_stack() {
   print_macro_ctx(i, macro_ident, macro_tok_lst, macro_args);
   putstr("\n################################\n");
 }
+
+#endif // DEBUG_CPP
