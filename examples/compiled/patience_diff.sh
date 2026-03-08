a6674d7c93c6b83efc1e6570e1c26fa9349e3cb6
Laurent Huberdeau <laurenthuberd@gmail.com>
2026-03-07 21:47:29 -0500

    Delete dead examples

--- examples/compiled/patience_diff.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e -u -f
-LC_ALL=C
-
-#_ DiffLine struct member declarations
-readonly __text=0
-readonly __type=1
-readonly __sizeof__DiffLine=2
-examples/patience_diff.c:18:8: Parse error: identifier, literal, or '(' expected
