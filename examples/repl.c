307fe58050b861a82c469874fdf740f6819ffb2f
Laurent Huberdeau <laurenthuberd@gmail.com>
2026-03-07 21:47:15 -0500

    Fix and regenerate repl example

--- examples/repl.c
+++ examples/repl.c
@@ -524,7 +524,7 @@ obj prim(int no) {
     buffer[0] = (char) NUM(x);
     success = write(NUM(y), buffer, 1);
     if (success != 1) {
-      perror("Cannot write to file.");
+      putstr("Cannot write to file.");
     }
     push2(TRUE, PAIR_TAG);
   }
